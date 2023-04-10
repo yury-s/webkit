c0fc84b22150394b898e97233c881a60b7cd1584
playwright-devops <devops@playwright.dev>
2023-04-10 15:07:04 -0700

    chore(webkit): bootstrap build #1827

--- Source/WebKit/Shared/WebPageCreationParameters.h
+++ Source/WebKit/Shared/WebPageCreationParameters.h
@@ -273,6 +273,8 @@ struct WebPageCreationParameters {
     
     bool httpsUpgradeEnabled { true };
 
+    bool shouldPauseInInspectorWhenShown { false };
+
 #if PLATFORM(IOS)
     bool allowsDeprecatedSynchronousXMLHttpRequestDuringUnload { false };
 #endif
