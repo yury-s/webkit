88aa9e305f89e64432fd6b3100a6c31b3cf272f7
Yury Semikhatsky <yurys@chromium.org>
2023-07-12 17:29:30 -0700

    Return cookies in the order of addition

--- Source/WebKit/UIProcess/InspectorPlaywrightAgent.cpp
+++ Source/WebKit/UIProcess/InspectorPlaywrightAgent.cpp
@@ -750,7 +750,13 @@ void InspectorPlaywrightAgent::getAllCookies(const String& browserContextID, Ref
             if (!callback->isActive())
                 return;
             auto cookies = JSON::ArrayOf<Inspector::Protocol::Playwright::Cookie>::create();
+
+// Soup returns cookies in the reverse order.
+#if USE(SOUP)
+            for (const auto& cookie : makeReversedRange(allCookies))
+#else
             for (const auto& cookie : allCookies)
+#endif
                 cookies->addItem(buildObjectForCookie(cookie));
             callback->sendSuccess(WTFMove(cookies));
         });
