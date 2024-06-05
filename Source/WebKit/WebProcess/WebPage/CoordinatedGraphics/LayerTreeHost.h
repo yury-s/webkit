199d091b62b3a089bfbbccd3a98105faf0ca5c93
playwright-devops <devops@playwright.dev>
2024-06-05 10:29:20 -0700

    chore(webkit): bootstrap build #2020

--- Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/LayerTreeHost.h
+++ Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/LayerTreeHost.h
@@ -115,6 +115,13 @@ public:
 #if PLATFORM(WPE) && USE(GBM) && ENABLE(WPE_PLATFORM)
     void preferredBufferFormatsDidChange();
 #endif
+
+// Playwright begin
+#if USE(COORDINATED_GRAPHICS)
+    const SimpleViewportController& viewportController() const { return m_viewportController; }
+#endif
+// Playwright end
+
 private:
 #if USE(COORDINATED_GRAPHICS)
     void layerFlushTimerFired();
