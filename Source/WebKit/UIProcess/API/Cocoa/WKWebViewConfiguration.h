fe1d29be3db233e0380cab89a797b00d9e307a61
Richard Robinson <richard_robinson2@apple.com>
2024-12-20 12:49:10 -0800

    Fix the build for some internal configurations https://bugs.webkit.org/show_bug.cgi?id=284998 rdar://140412673
    Reviewed by Abrar Rahman Protyasha.
    
    Fix a build dependency order issue by using a temporary workaround until it is no longer needed.
    
    * Source/WebKit/UIProcess/API/Cocoa/WKWebView.h:
    * Source/WebKit/UIProcess/API/Cocoa/WKWebViewConfiguration.h:
    
    Canonical link: https://commits.webkit.org/288184@main

--- Source/WebKit/UIProcess/API/Cocoa/WKWebViewConfiguration.h
+++ Source/WebKit/UIProcess/API/Cocoa/WKWebViewConfiguration.h
@@ -232,11 +232,11 @@ on the system setting.
     */
 @property (nonatomic) BOOL supportsAdaptiveImageGlyph WK_API_AVAILABLE(macos(15.0), ios(18.0), visionos(2.0));
 
-#if (TARGET_OS_IOS && !TARGET_OS_VISION) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 180000
+#if (TARGET_OS_IOS && __IPHONE_OS_VERSION_MAX_ALLOWED >= 180000) || (defined(TARGET_OS_VISION) && TARGET_OS_VISION && __has_include(<GameKit/GKReleaseState.h>))
 /*! @abstract The preferred behavior of Writing Tools.
     @discussion The default behavior is equivalent to `UIWritingToolsBehaviorLimited`.
     */
-@property (nonatomic) UIWritingToolsBehavior writingToolsBehavior WK_API_AVAILABLE(ios(18.0));
+@property (nonatomic) UIWritingToolsBehavior writingToolsBehavior WK_API_AVAILABLE(ios(18.0), visionos(WK_XROS_TBA));
 #elif TARGET_OS_OSX && __MAC_OS_X_VERSION_MAX_ALLOWED >= 150000
 /*! @abstract The preferred behavior of Writing Tools.
     @discussion The default behavior is equivalent to `NSWritingToolsBehaviorLimited`.
