e3000bee93ae8fdd028449fb382895a068d792c6
Samuel Thibault <samuel.thibault@ens-lyon.org>
2025-01-12 10:17:15 -0800

    Also use glibc mcontext_t fields on GNU/Hurd https://bugs.webkit.org/show_bug.cgi?id=285806
    Reviewed by Michael Catanzaro.
    
    GNU/Hurd also uses glibc, with the same mcontext_t structure and fields.
    
    * Source/JavaScriptCore/runtime/MachineContext.h:
    (JSC::MachineContext::stackPointerImpl):
    (JSC::MachineContext::framePointerImpl):
    (JSC::MachineContext::instructionPointerImpl):
    (JSC::MachineContext::argumentPointer<1>):
    (JSC::MachineContext::llintInstructionPointer):
    
    Canonical link: https://commits.webkit.org/288773@main

--- Source/JavaScriptCore/runtime/MachineContext.h
+++ Source/JavaScriptCore/runtime/MachineContext.h
@@ -196,7 +196,7 @@ static inline void*& stackPointerImpl(mcontext_t& machineContext)
 #error Unknown Architecture
 #endif
 
-#elif OS(FUCHSIA) || OS(LINUX)
+#elif OS(FUCHSIA) || OS(LINUX) || OS(HURD)
 
 #if CPU(X86)
     return reinterpret_cast<void*&>((uintptr_t&) machineContext.gregs[REG_ESP]);
@@ -341,7 +341,7 @@ static inline void*& framePointerImpl(mcontext_t& machineContext)
 #error Unknown Architecture
 #endif
 
-#elif OS(FUCHSIA) || OS(LINUX)
+#elif OS(FUCHSIA) || OS(LINUX) || OS(HURD)
 
 // The following sequence depends on glibc's sys/ucontext.h.
 #if CPU(X86)
@@ -521,7 +521,7 @@ static inline void*& instructionPointerImpl(mcontext_t& machineContext)
 #error Unknown Architecture
 #endif
 
-#elif OS(FUCHSIA) || OS(LINUX)
+#elif OS(FUCHSIA) || OS(LINUX) || OS(HURD)
 
 // The following sequence depends on glibc's sys/ucontext.h.
 #if CPU(X86)
@@ -686,7 +686,7 @@ inline void*& argumentPointer<1>(mcontext_t& machineContext)
 #error Unknown Architecture
 #endif
 
-#elif OS(FUCHSIA) || OS(LINUX)
+#elif OS(FUCHSIA) || OS(LINUX) || OS(HURD)
 
 // The following sequence depends on glibc's sys/ucontext.h.
 #if CPU(X86)
@@ -830,7 +830,7 @@ inline void*& llintInstructionPointer(mcontext_t& machineContext)
 #error Unknown Architecture
 #endif
 
-#elif OS(FUCHSIA) || OS(LINUX)
+#elif OS(FUCHSIA) || OS(LINUX) || OS(HURD)
 
 // The following sequence depends on glibc's sys/ucontext.h.
 #if CPU(X86)
