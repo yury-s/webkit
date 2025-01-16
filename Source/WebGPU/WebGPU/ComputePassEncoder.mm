46c6dd5414b60c3c64c27c688ad9b625321007c4
Mike Wyrzykowski <mwyrzykowski@apple.com>
2025-01-15 20:42:42 -0800

    [WebGPU] Switching pipelines does not zero out dynamic offsets from prior pipeline https://bugs.webkit.org/show_bug.cgi?id=285828 rdar://142761614
    Reviewed by Cameron McCormack.
    
    When calling resize() on a Vector, dynamic offsets from a previous draw
    call could remain in the dynamic offsets Vector. Ensure they are zero'ed instead
    by using Vector's ctor instead.
    
    No change needed for RenderBundles since render bundles save all the dynamic
    offsets across all draw calls into a single buffer.
    
    * LayoutTests/fast/webgpu/nocrash/fuzz-285828-expected.txt: Added.
    * LayoutTests/fast/webgpu/nocrash/fuzz-285828.html: Added.
    Add regression test.
    
    * Source/WebGPU/WebGPU/ComputePassEncoder.mm:
    (WebGPU::ComputePassEncoder::setPipeline):
    * Source/WebGPU/WebGPU/RenderPassEncoder.mm:
    (WebGPU::RenderPassEncoder::setPipeline):
    Ensure stale dynamic offsets do not persist.
    
    Canonical link: https://commits.webkit.org/288990@main

--- Source/WebGPU/WebGPU/ComputePassEncoder.mm
+++ Source/WebGPU/WebGPU/ComputePassEncoder.mm
@@ -483,6 +483,7 @@ void ComputePassEncoder::setPipeline(const ComputePipeline& pipeline)
 
     m_pipeline = &pipeline;
     m_computeDynamicOffsets.resize(m_pipeline->protectedPipelineLayout()->sizeOfComputeDynamicOffsets());
+    m_computeDynamicOffsets.fill(0);
 
     ASSERT(pipeline.computePipelineState());
     m_threadsPerThreadgroup = pipeline.threadsPerThreadgroup();
