yury-s/webkit

commit 0f12ab8938f6d250560003ba3e43215aae7c0934

Convert color types to strongly typed CSS/Style values https://bugs.webkit.org/show_bug.cgi?id=283828
Reviewed by Darin Adler.

Integrates color types into the strongly typed CSS/Style value architecture.

- Renames all CSSUnresolved*Foo color types to CSS::Foo and moves them to css/values/color/.
- Renames all Style*Foo color types to Style::Foo and moves them to style/values/color/.
- Swaps which types use UniqueRefs in CSS::Color to match Style::Color. This is needed
  because now CSS color values are stored in a new CSSColorValue type instead of being
  allocated and and referenced from a CSSPrimitiveValue. The CSSValuePool now vends/caches
  CSSColorValues for colors created through it.
- Implements standard interfaces for strongly type CSS/Style values for color types
  (e.g. Serialization<>, ComputedStyleDependenciesCollector<>, ToStyle<>/ToCSS<>, etc.).
- Moves color keyword lookup to CSSKeywordColor from StyleColor.
- Adopts CSS::Color in CSS::GradientColorStop to prove things out. To better model the
  semantics of color stops, the color value is now generic, allowing Angular/Linear
  stops to use Markable<CSS::Color> whereas Deprecated stops use CSS::Color directly,
  since they are required at parse time.
- To avoid name collisions, the typedom CSS color types where updated to use the prefix
  CSSOM rather than CSS, matching precedent set in CSSOMVariableReferenceValue.

* Source/WebCore/CMakeLists.txt:
* Source/WebCore/DerivedSources-input.xcfilelist:
* Source/WebCore/DerivedSources-output.xcfilelist:
* Source/WebCore/DerivedSources.make:
* Source/WebCore/Headers.cmake:
* Source/WebCore/SaferCPPExpectations/NoUncountedMemberCheckerExpectations:
* Source/WebCore/SaferCPPExpectations/UncountedCallArgsCheckerExpectations:
* Source/WebCore/Sources.txt:
* Source/WebCore/WebCore.xcodeproj/project.pbxproj:
* Source/WebCore/animation/CSSPropertyAnimation.cpp:
* Source/WebCore/bindings/js/WebCoreBuiltinNames.h:
* Source/WebCore/css/CSSColorValue.cpp: Added.

Source/WebCore/style/values/color/StyleColor.h
@@ -1,0 +1,227 @@
+/*
+ * Copyright (C) 2013 Google Inc. All rights reserved.
+ * Copyright (C) 2016-2023 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "CSSColor.h"
+#include "CSSColorDescriptors.h"
+#include "CSSColorType.h"
+#include "CSSValueKeywords.h"
+#include "StyleColorOptions.h"
+#include "StyleCurrentColor.h"
+#include "StyleResolvedColor.h"
+#include <wtf/Markable.h>
+#include <wtf/OptionSet.h>
+#include <wtf/UniqueRef.h>
+
+namespace WebCore {
+
+class Color;
+class Document;
+class RenderStyle;
+
+namespace Style {
+
+enum class ForVisitedLink : bool;
+
+// The following style color kinds are forward declared and stored in
+// UniqueRefs to avoid unnecessarily growing the size of Color for the
+// uncommon case of un-resolvability due to currentColor.
+struct ColorLayers;
+struct ColorMix;
+struct ContrastColor;
+template<typename Descriptor> struct RelativeColor;
+
+struct Color {
+private:
+    struct EmptyToken { constexpr bool operator==(const EmptyToken&) const = default; };
+
+    // FIXME: Replace std::variant with a generic CompactPointerVariant type.
+    using ColorKind = std::variant<
+        EmptyToken,
+        ResolvedColor,
+        CurrentColor,
+        UniqueRef<ColorLayers>,
+        UniqueRef<ColorMix>,
+        UniqueRef<ContrastColor>,
+        UniqueRef<RelativeColor<RGBFunctionModernRelative>>,
+        UniqueRef<RelativeColor<HSLFunctionModern>>,
+        UniqueRef<RelativeColor<HWBFunction>>,
+        UniqueRef<RelativeColor<LabFunction>>,
+        UniqueRef<RelativeColor<LCHFunction>>,
+        UniqueRef<RelativeColor<OKLabFunction>>,
+        UniqueRef<RelativeColor<OKLCHFunction>>,
+        UniqueRef<RelativeColor<ColorRGBFunction<ExtendedA98RGB<float>>>>,
+        UniqueRef<RelativeColor<ColorRGBFunction<ExtendedDisplayP3<float>>>>,
+        UniqueRef<RelativeColor<ColorRGBFunction<ExtendedProPhotoRGB<float>>>>,
+        UniqueRef<RelativeColor<ColorRGBFunction<ExtendedRec2020<float>>>>,
+        UniqueRef<RelativeColor<ColorRGBFunction<ExtendedSRGBA<float>>>>,
+        UniqueRef<RelativeColor<ColorRGBFunction<ExtendedLinearSRGBA<float>>>>,
+        UniqueRef<RelativeColor<ColorXYZFunction<XYZA<float, WhitePoint::D50>>>>,
+        UniqueRef<RelativeColor<ColorXYZFunction<XYZA<float, WhitePoint::D65>>>>
+    >;
+
+    Color(EmptyToken);
+    Color(ColorKind&&);
+
+public:
+    // The default constructor initializes to Style::CurrentColor to preserve old behavior,
+    // we might want to remove it entirely at some point.
+    Color();
+
+    // Convenience constructors that create Style::ResolvedColor.
+    Color(WebCore::Color);
+    Color(SRGBA<uint8_t>);
+
+    Color(ResolvedColor&&);
+    Color(CurrentColor&&);
+    Color(ColorLayers&&);
+    Color(ColorMix&&);
+    Color(ContrastColor&&);
+    Color(RelativeColor<RGBFunctionModernRelative>&&);
+    Color(RelativeColor<HSLFunctionModern>&&);
+    Color(RelativeColor<HWBFunction>&&);
+    Color(RelativeColor<LabFunction>&&);
+    Color(RelativeColor<LCHFunction>&&);
+    Color(RelativeColor<OKLabFunction>&&);
+    Color(RelativeColor<OKLCHFunction>&&);
+    Color(RelativeColor<ColorRGBFunction<ExtendedA98RGB<float>>>&&);
+    Color(RelativeColor<ColorRGBFunction<ExtendedDisplayP3<float>>>&&);
+    Color(RelativeColor<ColorRGBFunction<ExtendedProPhotoRGB<float>>>&&);
+    Color(RelativeColor<ColorRGBFunction<ExtendedRec2020<float>>>&&);
+    Color(RelativeColor<ColorRGBFunction<ExtendedSRGBA<float>>>&&);
+    Color(RelativeColor<ColorRGBFunction<ExtendedLinearSRGBA<float>>>&&);
+    Color(RelativeColor<ColorXYZFunction<XYZA<float, WhitePoint::D50>>>&&);
+    Color(RelativeColor<ColorXYZFunction<XYZA<float, WhitePoint::D65>>>&&);
+
+    WEBCORE_EXPORT Color(const Color&);
+    Color& operator=(const Color&);
+
+    Color(Color&&);
+    Color& operator=(Color&&);
+
+    WEBCORE_EXPORT ~Color();
+
+    bool operator==(const Color&) const;
+
+    static Color currentColor();
+
+    bool containsCurrentColor() const;
+    bool isCurrentColor() const;
+    bool isColorMix() const;
+    bool isContrastColor() const;
+    bool isRelativeColor() const;
+
+    bool isResolvedColor() const;
+    const WebCore::Color& resolvedColor() const;
+
+    WEBCORE_EXPORT WebCore::Color resolveColor(const WebCore::Color& currentColor) const;
+
+    // This helper allows us to treat all the alternatives in ColorKind
+    // as const references, pretending the UniqueRefs don't exist.
+    template<typename... F> decltype(auto) switchOn(F&&...) const;
+
+    struct MarkableTraits {
+        static bool isEmptyValue(const Color&);
+        static Color emptyValue();
+    };
+
+    String debugDescription() const;
+
+private:
+    template<typename T>
+    static ColorKind makeIndirectColor(T&&);
+    static ColorKind copy(const ColorKind&);
+
+    ColorKind value;
+};
+
+WebCore::Color resolveColor(const Color&, const WebCore::Color& currentColor);
+bool containsCurrentColor(const Color&);
+
+void serializationForCSS(StringBuilder&, const Color&);
+WEBCORE_EXPORT String serializationForCSS(const Color&);
+
+WTF::TextStream& operator<<(WTF::TextStream&, const Color&);
+
+// MARK: - Conversion
+
+Color toStyleColor(const CSS::Color&, ColorResolutionState&);
+Color toStyleColor(const CSS::Color&, Ref<const Document>, const RenderStyle&, const CSSToLengthConversionData&, ForVisitedLink);
+Color toStyleColorWithResolvedCurrentColor(const CSS::Color&, Ref<const Document>, RenderStyle&, const CSSToLengthConversionData&, ForVisitedLink);
+
+template<> struct ToCSS<Color> {
+    auto operator()(const Color&, const RenderStyle&) -> CSS::Color;
+};
+template<> struct ToStyle<CSS::Color> {
+    auto operator()(const CSS::Color&, const BuilderState&, const CSSCalcSymbolTable&, ForVisitedLink) -> Color;
+    auto operator()(const CSS::Color&, const BuilderState&, const CSSCalcSymbolTable&) -> Color;
+};
+
+template<typename... F> decltype(auto) Color::switchOn(F&&... f) const
+{
+    auto visitor = WTF::makeVisitor(std::forward<F>(f)...);
+    using ResultType = decltype(visitor(std::declval<ResolvedColor>()));
+
+    return WTF::switchOn(value,
+        [&](const EmptyToken&) -> ResultType {
+            RELEASE_ASSERT_NOT_REACHED();
+        },
+        [&](const ResolvedColor& resolvedColor) -> ResultType {
+            return visitor(resolvedColor);
+        },
+        [&](const CurrentColor& currentColor) -> ResultType {
+            return visitor(currentColor);
+        },
+        [&]<typename T>(const UniqueRef<T>& color) -> ResultType {
+            return visitor(color.get());
+        }
+    );
+}
+
+} // namespace Style
+} // namespace WebCore
+
+namespace WTF {
+
+// Overload WTF::switchOn to make it so Style::Color can be used directly.
+
+template<class... F> ALWAYS_INLINE auto switchOn(const WebCore::Style::Color& value, F&&... f) -> decltype(value.switchOn(std::forward<F>(f)...))
+{
+    return value.switchOn(std::forward<F>(f)...);
+}
+
+template<class... F> ALWAYS_INLINE auto switchOn(WebCore::Style::Color&& value, F&&... f) -> decltype(value.switchOn(std::forward<F>(f)...))
+{
+    return value.switchOn(std::forward<F>(f)...);
+}
+
+} // namespace WTF

Source/WebCore/style/values/images/StyleGradient.cpp
@@ -59,33 +59,11 @@ template<CSSValueID Name, typename T> static constexpr bool isRepeating(const Fu
 
 // MARK: - Conversion: Style -> CSS
 
-static RefPtr<CSSPrimitiveValue> toCSSColorStopColor(const std::optional<StyleColor>& color, const RenderStyle& style)
-{
-    if (!color)
-        return nullptr;
-    return ComputedStyleExtractor::currentColorOrValidColor(style, *color);
-}
-
-static auto toCSSColorStopPosition(const GradientLinearColorStop::Position& position, const RenderStyle& style) -> CSS::GradientLinearColorStopPosition
-{
-    return toCSS(position, style);
-}
-
-static auto toCSSColorStopPosition(const GradientAngularColorStop::Position& position, const RenderStyle& style) -> CSS::GradientAngularColorStopPosition
-{
-    return toCSS(position, style);
-}
-
-static auto toCSSColorStopPosition(const GradientDeprecatedColorStop::Position& position, const RenderStyle& style) -> CSS::GradientDeprecatedColorStopPosition
-{
-    return toCSS(position, style);
-}
-
 template<typename CSSStop, typename StyleStop> static auto toCSSColorStop(const StyleStop& stop, const RenderStyle& style) -> CSSStop
 {
     return CSSStop {
-        toCSSColorStopColor(stop.color, style),
-        toCSSColorStopPosition(stop.position, style)
+        toCSS(stop.color, style),
+        toCSS(stop.position, style)
     };
 }
 
@@ -106,17 +84,10 @@ auto ToCSS<GradientDeprecatedColorStop>::operator()(const GradientDeprecatedColo
 
 // MARK: - Conversion: CSS -> Style
 
-static auto toStyleStopColor(const RefPtr<CSSPrimitiveValue>& color, const BuilderState& state, const CSSCalcSymbolTable&) -> std::optional<StyleColor>
-{
-    if (!color)
-        return std::nullopt;
-    return state.colorFromPrimitiveValue(*color);
-}
-
 template<typename T> decltype(auto) toStyleColorStop(const T& stop, const BuilderState& state, const CSSCalcSymbolTable& symbolTable)
 {
     return GradientColorStop {
-        toStyleStopColor(stop.color, state, symbolTable),
+        toStyle(stop.color, state, symbolTable),
         toStyle(stop.position, state, symbolTable)
     };
 }
@@ -138,14 +109,18 @@ auto ToStyle<CSS::GradientDeprecatedColorStop>::operator()(const CSS::GradientDe
 
 // MARK: - Platform Gradient Resolution
 
-static Color resolveColorStopColor(const std::optional<StyleColor>& styleColor, const RenderStyle& style, bool hasColorFilter)
+static WebCore::Color resolveColorStopColor(const Color& styleColor, const RenderStyle& style, bool hasColorFilter)
+{
+    if (hasColorFilter)
+        return style.colorWithColorFilter(styleColor);
+    return style.colorResolvingCurrentColor(styleColor);
+}
+
+static WebCore::Color resolveColorStopColor(const Markable<Color>& styleColor, const RenderStyle& style, bool hasColorFilter)
 {
     if (!styleColor)
         return { };
-
-    if (hasColorFilter)
-        return style.colorWithColorFilter(*styleColor);
-    return style.colorResolvingCurrentColor(*styleColor);
+    return resolveColorStopColor(*styleColor, style, hasColorFilter);
 }
 
 static std::optional<float> resolveColorStopPosition(const GradientLinearColorStop::Position& position, float gradientLength)
@@ -194,7 +169,7 @@ static float resolveColorStopPosition(const GradientDeprecatedColorStop::Positio
 }
 
 struct ResolvedGradientStop {
-    Color color;
+    WebCore::Color color;
     std::optional<float> offset;
 
     bool isSpecified() const { return offset.has_value(); }
@@ -490,8 +465,8 @@ template<typename GradientAdapter, typename StyleGradient> GradientColorStops co
 
         // Find previous and next color so we know what to interpolate between.
         // We already know they have a color since we checked for that earlier.
-        Color color1 = stops[x - 1].color;
-        Color color2 = stops[x + 1].color;
+        auto color1 = stops[x - 1].color;
+        auto color2 = stops[x + 1].color;
         // Likewise find the position of previous and next color stop.
         float offset1 = *stops[x - 1].offset;
         float offset2 = *stops[x + 1].offset;
@@ -1211,10 +1186,20 @@ Ref<WebCore::Gradient> createPlatformGradient(const Gradient& gradient, const Fl
 
 // MARK: - stopsAreCacheable
 
+static bool stopColorIsCacheable(const Color& stopColor)
+{
+    return !containsCurrentColor(stopColor);
+}
+
+static bool stopColorIsCacheable(const Markable<Color>& stopColor)
+{
+    return !stopColor || stopColorIsCacheable(*stopColor);
+}
+
 template<typename Gradient> static bool stopsAreCacheable(const Gradient& gradient)
 {
     return std::ranges::none_of(gradient.parameters.stops, [](auto& stop) {
-        return stop.color && stop.color->containsCurrentColor();
+        return stopColorIsCacheable(stop.color);
     });
 }
 

commit f88290ea41bc0d62dcb4a581dfbcd787d4664796

StyleGradientImages never cache their image https://bugs.webkit.org/show_bug.cgi?id=286186
Reviewed by Simon Fraser.

StyleGradientImage backing store caching has been broken since 287234@main.
The fact we were never caching was also masking an issue where we would fail
to draw anything if a cacheable StyleGradientImage was being used as the
input to another StyleImage, such as StyleFilterImage.

* Source/WebCore/style/values/images/StyleGradient.cpp:
     - Fix check for cacheability by updating `none_of` -> `all_of`.
     - Remove unnecessary check for containing the client. Since the
       gradient is cacheable and cannot change we don't care about
       the client here like we do for StyleCanvasImage where client
       must be notified on changes. Removing this check allows
       gradients used by other StyleImages (like StyleFilterImage)
       to be successfully cached.

* Source/WebCore/style/values/color/StyleColor.h:
    - Export some functions to make unit test work.

* Tools/TestWebKitAPI/CMakeLists.txt:
* Tools/TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
* Tools/TestWebKitAPI/Tests/WebCore/StyleGradient.cpp: Added.
    - Add unit tests for both cacheable and uncacheable states.

Canonical link: https://commits.webkit.org/289123@main

Source/WebCore/rendering/style/StyleGradientImage.cpp
@@ -83,8 +83,6 @@ RefPtr<Image> StyleGradientImage::image(const RenderElement* renderer, const Flo
 
     bool cacheable = m_knownCacheableBarringFilter && !style.hasAppleColorFilter();
     if (cacheable) {
-        if (!clients().contains(const_cast<RenderElement&>(*renderer)))
-            return nullptr;
         if (auto* result = const_cast<StyleGradientImage&>(*this).cachedImageForSize(size))
             return result;
     }

Source/WebCore/style/values/color/StyleColor.h
@@ -101,8 +101,8 @@ public:
     Color(WebCore::Color);
     Color(SRGBA<uint8_t>);
 
-    Color(ResolvedColor&&);
-    Color(CurrentColor&&);
+    WEBCORE_EXPORT Color(ResolvedColor&&);
+    WEBCORE_EXPORT Color(CurrentColor&&);
     Color(ColorLayers&&);
     Color(ColorMix&&);
     Color(ContrastColor&&);
@@ -125,7 +125,7 @@ public:
     WEBCORE_EXPORT Color(const Color&);
     Color& operator=(const Color&);
 
-    Color(Color&&);
+    WEBCORE_EXPORT Color(Color&&);
     Color& operator=(Color&&);
 
     WEBCORE_EXPORT ~Color();

Source/WebCore/style/values/images/StyleGradient.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2022 Apple Inc. All rights reserved.
- * Copyright (C) 2024 Samuel Weinig <sam@webkit.org>
+ * Copyright (C) 2024-2025 Samuel Weinig <sam@webkit.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -1198,7 +1198,7 @@ static bool stopColorIsCacheable(const Markable<Color>& stopColor)
 
 template<typename Gradient> static bool stopsAreCacheable(const Gradient& gradient)
 {
-    return std::ranges::none_of(gradient.parameters.stops, [](auto& stop) {
+    return std::ranges::all_of(gradient.parameters.stops, [](auto& stop) {
         return stopColorIsCacheable(stop.color);
     });
 }

Source/WebCore/style/values/images/StyleGradient.h
@@ -390,7 +390,7 @@ using Gradient = std::variant<
 Ref<WebCore::Gradient> createPlatformGradient(const Gradient&, const FloatSize&, const RenderStyle&);
 
 // Returns whether it caching based on the gradient's stops is allowed.
-bool stopsAreCacheable(const Gradient&);
+WEBCORE_EXPORT bool stopsAreCacheable(const Gradient&);
 
 // Returns whether the gradient is opaque.
 bool isOpaque(const Gradient&, const RenderStyle&);

Tools/TestWebKitAPI/CMakeLists.txt
@@ -222,6 +222,7 @@ if (ENABLE_WEBCORE)
         Tests/WebCore/SecurityOrigin.cpp
         Tests/WebCore/SharedBuffer.cpp
         Tests/WebCore/SharedBufferTest.cpp
+        Tests/WebCore/StyleGradient.cpp
         Tests/WebCore/TimeRanges.cpp
         Tests/WebCore/TransformationMatrix.cpp
         Tests/WebCore/URLParserTextEncoding.cpp

Tools/TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj
@@ -1187,6 +1187,7 @@
 		BCB68042126FBFF100642A61 /* DocumentStartUserScriptAlertCrash_Bundle.cpp in Sources */ = {isa = PBXBuildFile; fileRef = BCB68041126FBFF100642A61 /* DocumentStartUserScriptAlertCrash_Bundle.cpp */; };
 		BCE0DFB72D188ADC003F0349 /* CompactVariant.cpp in Sources */ = {isa = PBXBuildFile; fileRef = BCE0DFB62D188ADC003F0349 /* CompactVariant.cpp */; };
 		BCEA080E2CD00F5C000AC148 /* VariantList.cpp in Sources */ = {isa = PBXBuildFile; fileRef = BCEA080D2CD00F5C000AC148 /* VariantList.cpp */; };
+		BCED51762D3B3C1200C57E2D /* StyleGradient.cpp in Sources */ = {isa = PBXBuildFile; fileRef = BCED51752D3B3C1200C57E2D /* StyleGradient.cpp */; };
 		C0BD669F131D3CFF00E18F2A /* ResponsivenessTimerDoesntFireEarly_Bundle.cpp in Sources */ = {isa = PBXBuildFile; fileRef = C0BD669E131D3CFF00E18F2A /* ResponsivenessTimerDoesntFireEarly_Bundle.cpp */; };
 		C0C5D3C61459912900A802A6 /* GetBackingScaleFactor_Bundle.mm in Sources */ = {isa = PBXBuildFile; fileRef = C0C5D3BD14598B6F00A802A6 /* GetBackingScaleFactor_Bundle.mm */; };
 		C104BC1F2547237100C078C9 /* OverrideAppleLanguagesPreference.mm in Sources */ = {isa = PBXBuildFile; fileRef = C104BC1E2547237100C078C9 /* OverrideAppleLanguagesPreference.mm */; };
@@ -3417,6 +3418,7 @@
 		BCC8B95A12611F4700DE46A4 /* FailedLoad.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = FailedLoad.cpp; sourceTree = "<group>"; };
 		BCE0DFB62D188ADC003F0349 /* CompactVariant.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; path = CompactVariant.cpp; sourceTree = "<group>"; };
 		BCEA080D2CD00F5C000AC148 /* VariantList.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; path = VariantList.cpp; sourceTree = "<group>"; };
+		BCED51752D3B3C1200C57E2D /* StyleGradient.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; path = StyleGradient.cpp; sourceTree = "<group>"; };
 		C02B77F1126612140026BF0F /* SpacebarScrolling.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = SpacebarScrolling.cpp; sourceTree = "<group>"; };
 		C02B7853126613AE0026BF0F /* Carbon.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; path = Carbon.framework; sourceTree = SDKROOT; };
 		C02B7882126615410026BF0F /* spacebar-scrolling.html */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.html; path = "spacebar-scrolling.html"; sourceTree = "<group>"; };
@@ -4774,6 +4776,7 @@
 				ECA680CD1E68CC0900731D20 /* StringUtilities.mm */,
 				CE4D5DE51F6743BA0072CFC6 /* StringWithDirection.cpp */,
 				41E67A8425D16E83007B0A4C /* STUNMessageParsingTest.cpp */,
+				BCED51752D3B3C1200C57E2D /* StyleGradient.cpp */,
 				1C9DD9222697655B00274DB2 /* SVGImageCasts.cpp */,
 				155B872B2BF55D7800D93968 /* TextBoundaries.cpp */,
 				93A258981F92FF15003E510C /* TextCodec.cpp */,
@@ -7260,6 +7263,7 @@
 				1C8FA53A2685A6D500B7E49E /* StringWidth.mm in Sources */,
 				CE4D5DE71F6743BA0072CFC6 /* StringWithDirection.cpp in Sources */,
 				41E67A8525D16E83007B0A4C /* STUNMessageParsingTest.cpp in Sources */,
+				BCED51762D3B3C1200C57E2D /* StyleGradient.cpp in Sources */,
 				7CCE7ED21A411A7E00447C4C /* SubresourceErrorCrash.mm in Sources */,
 				51EB126724CB8753000CB030 /* SunLightApplicationGenericNES.mm in Sources */,
 				1C9DD9232697655B00274DB2 /* SVGImageCasts.cpp in Sources */,

Tools/TestWebKitAPI/Tests/WebCore/StyleGradient.cpp
@@ -1,0 +1,102 @@
+/*
+ * Copyright (C) 2025 Samuel Weinig <sam@webkit.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "config.h"
+
+#include "Test.h"
+#include <WebCore/StyleGradient.h>
+
+namespace TestWebKitAPI {
+
+static WebCore::Style::GradientLinearColorStopList cacheableStops()
+{
+    return {
+        {
+            WebCore::Style::Color { WebCore::Style::ResolvedColor { WebCore::Color::red } },
+            WebCore::Style::Percentage<> { 50 }
+        },
+        {
+            WebCore::Style::Color { WebCore::Style::ResolvedColor { WebCore::Color::blue } },
+            WebCore::Style::Percentage<> { 100 }
+        }
+    };
+}
+
+static WebCore::Style::GradientLinearColorStopList someUncacheableStops()
+{
+    return {
+        {
+            WebCore::Style::Color { WebCore::Style::CurrentColor { } },
+            WebCore::Style::Percentage<> { 50 }
+        },
+        {
+            WebCore::Style::Color { WebCore::Style::ResolvedColor { WebCore::Color::blue } },
+            WebCore::Style::Percentage<> { 100 }
+        }
+    };
+}
+
+static WebCore::Style::GradientLinearColorStopList allUncacheableStops()
+{
+    return {
+        {
+            WebCore::Style::Color { WebCore::Style::CurrentColor { } },
+            WebCore::Style::Percentage<> { 50 }
+        },
+        {
+            WebCore::Style::Color { WebCore::Style::CurrentColor { } },
+            WebCore::Style::Percentage<> { 100 }
+        }
+    };
+}
+
+static WebCore::Style::Gradient gradientWithStops(WebCore::Style::GradientLinearColorStopList stops)
+{
+    return WebCore::FunctionNotation<WebCore::CSSValueLinearGradient, WebCore::Style::LinearGradient> {
+        .parameters = {
+            .colorInterpolationMethod = WebCore::CSS::GradientColorInterpolationMethod {
+                .method = { WebCore::ColorInterpolationMethod::SRGB { }, WebCore::AlphaPremultiplication::Premultiplied },
+                .defaultMethod = WebCore::CSS::GradientColorInterpolationMethod::Default::SRGB,
+            },
+            .gradientLine = WebCore::CSS::Horizontal { WebCore::CSS::Keyword::Left { } },
+            .stops = stops
+        }
+    };
+}
+
+TEST(WebCore, StyleGradientStopsAreCacheable)
+{
+    auto cacheableGradient = gradientWithStops(cacheableStops());
+    ASSERT_TRUE(WebCore::Style::stopsAreCacheable(cacheableGradient));
+
+    auto uncacheableGradient1 = gradientWithStops(someUncacheableStops());
+    ASSERT_FALSE(WebCore::Style::stopsAreCacheable(uncacheableGradient1));
+
+    auto uncacheableGradient2 = gradientWithStops(allUncacheableStops());
+    ASSERT_FALSE(WebCore::Style::stopsAreCacheable(uncacheableGradient2));
+}
+
+} // namespace TestWebKitAPI