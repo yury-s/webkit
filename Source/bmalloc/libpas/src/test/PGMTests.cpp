1dc8717af8a79287e871a499e6ca3cbd32fde51b
Zak Ridouh <zakr@apple.com>
2025-01-21 18:16:25 -0800

    [libpas] Add alloc/dealloc stack traces to WebKit PGM https://bugs.webkit.org/show_bug.cgi?id=283370 rdar://106166552
    Reviewed by David Kilzer.
    
    This adds collection and storage of <=31 frames of backtrace upon PGM
    allocation and deallocation. A followup patch will pack these frame
    pointers more aggressivly.
    
    Additionally, bump `pas_crash_report_version` to version 3 so
    `reportcrashd` is aware of struct changes.
    
    Add a variety of tests to validate this new functionality.
    
    * Source/bmalloc/libpas/src/libpas/pas_probabilistic_guard_malloc_allocator.c:
    (pas_probabilistic_guard_malloc_allocate):
    (pas_probabilistic_guard_malloc_deallocate):
    * Source/bmalloc/libpas/src/libpas/pas_probabilistic_guard_malloc_allocator.h:
    * Source/bmalloc/libpas/src/libpas/pas_report_crash.c:
    (pas_update_report_crash_fields):
    (pas_report_crash_extract_pgm_failure):
    * Source/bmalloc/libpas/src/libpas/pas_report_crash_pgm_report.h:
    * Source/bmalloc/libpas/src/test/EnumerationTests.cpp:
    (std::testPGMEnumerationBasic):
    (std::testPGMEnumerationAddAndFree):
    * Source/bmalloc/libpas/src/test/PGMTests.cpp:
    (std::testPGMBmallocAllocationBacktrace):
    (std::testPGMAllocMetadataOnly):
    (addPGMTests):
    
    Canonical link: https://commits.webkit.org/289218@main

--- Source/bmalloc/libpas/src/test/PGMTests.cpp
+++ Source/bmalloc/libpas/src/test/PGMTests.cpp
@@ -20,14 +20,16 @@
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <unistd.h>
+#include <mach/arm/kern_return.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "TestHarness.h"
 
+#include "bmalloc/BPlatform.h"
 #include "bmalloc_heap.h"
 #include "iso_heap.h"
 #include "iso_heap_config.h"
@@ -37,6 +39,11 @@
 #include "pas_ptr_hash_map.h"
 #include "pas_report_crash.h"
 #include "pas_root.h"
+#include "pas_platform.h"
+
+#if !PAS_PLATFORM(PLAYSTATION)
+#include <execinfo.h>
+#endif // !PAS_PLATFORM(PLAYSTATION)
 
 using namespace std;
 
@@ -88,7 +95,7 @@ void testPGMMultipleAlloc() {
     size_t init_free_virtual_mem = pas_probabilistic_guard_malloc_get_free_virtual_memory();
     size_t init_free_wasted_mem = pas_probabilistic_guard_malloc_get_free_wasted_memory();
 
-    size_t num_allocations = 100;
+    const size_t num_allocations = 100;
     pas_allocation_result mem_storage[num_allocations];
 
     for (size_t i = 0; i < num_allocations; i++ ) {
@@ -325,6 +332,8 @@ void testPGMMetadataVectorManagementFewDeallocations() {
     pas_heap_lock_unlock();
 }
 
+/* Backtrace API is currently not supported on PlayStation. */
+#if !PAS_PLATFORM(PLAYSTATION)
 void testPGMMetadataDoubleFreeBehavior() {
     pas_probabilistic_guard_malloc_initialize_pgm_as_enabled();
 
@@ -364,6 +373,107 @@ void testPGMMetadataDoubleFreeBehavior() {
     pas_heap_lock_unlock();
 }
 
+void testPGMBmallocAllocationBacktrace() {
+    pas_probabilistic_guard_malloc_initialize_pgm_as_enabled();
+
+    pas_heap_lock_lock();
+    pas_root* root = pas_root_create();
+    pas_heap_lock_unlock();
+
+    // Allocate and check arrays of various sizes
+    int* int_arr1 = static_cast<int*>(bmalloc_allocate(30000 * sizeof(int), pas_non_compact_allocation_mode));
+    CHECK(int_arr1);
+
+    int* int_arr2 = static_cast<int*>(bmalloc_allocate(20000 * sizeof(int), pas_non_compact_allocation_mode));
+    CHECK(int_arr2);
+
+    bmalloc_deallocate(int_arr1);
+    bmalloc_deallocate(int_arr2);
+
+    int* int_arr3 = static_cast<int*>(bmalloc_allocate(499999 * sizeof(int), pas_non_compact_allocation_mode));
+    CHECK(int_arr3);
+
+    bmalloc_deallocate(int_arr3);
+
+    char* char_arr4 = static_cast<char*>(bmalloc_allocate(500000 * sizeof(char), pas_non_compact_allocation_mode));
+    CHECK(char_arr4);
+
+    char* char_arr5 = static_cast<char*>(bmalloc_allocate(399999 * sizeof(char), pas_non_compact_allocation_mode));
+    CHECK(char_arr5);
+    bmalloc_deallocate(char_arr5);
+
+    pas_ptr_hash_map* hash_map = root->pas_pgm_hash_map_instance;
+    CHECK(hash_map);
+
+    size_t table_size = hash_map->table_size;
+
+    // Check number of entries we have is 5
+    CHECK_EQUAL(hash_map->key_count, static_cast<size_t>(5));
+
+    // Traverse through hash_map entries
+    for (size_t i = 0; i < table_size; ++i) {
+        pas_ptr_hash_map_entry* hash_map_entry = &hash_map->table[i];
+
+        // Skip entry if key is invalid
+        if (hash_map_entry->key == (void*)UINTPTR_MAX)
+            continue;
+
+        pas_pgm_storage* pgm_metadata = static_cast<pas_pgm_storage*>(hash_map_entry->value);
+        if (!pgm_metadata)
+            continue;
+
+        const backtrace_metadata_t alloc_metadata = pgm_metadata->alloc_backtrace;
+        CHECK_GREATER(alloc_metadata.frame_size, 0);
+
+        const backtrace_metadata_t dealloc_metadata = pgm_metadata->dealloc_backtrace;
+        if (pgm_metadata->free_status)
+            CHECK_GREATER(dealloc_metadata.frame_size, 0);
+    }
+}
+
+void testPGMAllocMetadataOnly() {
+    pas_probabilistic_guard_malloc_initialize_pgm_as_enabled();
+
+    pas_heap_lock_lock();
+    pas_root* root = pas_root_create();
+    pas_heap_lock_unlock();
+
+    // Allocate and check arrays of various sizes
+    int* int_arr1 = static_cast<int*>(bmalloc_allocate(30000 * sizeof(int), pas_non_compact_allocation_mode));
+    CHECK(int_arr1);
+
+    pas_ptr_hash_map* hash_map = root->pas_pgm_hash_map_instance;
+    CHECK(hash_map);
+
+    size_t table_size = hash_map->table_size;
+
+    // Check number of entries we have is 1
+    CHECK_EQUAL(hash_map->key_count, static_cast<size_t>(1));
+
+    // Traverse through hash_map entries
+    for (size_t i = 0; i < table_size; ++i) {
+        pas_ptr_hash_map_entry* hash_map_entry = &hash_map->table[i];
+
+        // Skip entry if key is invalid
+        if (hash_map_entry->key == (void*)UINTPTR_MAX)
+            continue;
+
+        pas_pgm_storage* pgm_metadata = static_cast<pas_pgm_storage*>(hash_map_entry->value);
+        if (!pgm_metadata)
+            continue;
+
+        // Verify we do have metadata for an eallocation.
+        const backtrace_metadata_t alloc_metadata = pgm_metadata->alloc_backtrace;
+        CHECK_GREATER(alloc_metadata.frame_size, 0);
+
+        // Verify we do not have any metadata for a deallocation.
+        const backtrace_metadata_t dealloc_metadata = pgm_metadata->dealloc_backtrace;
+        CHECK(!pgm_metadata->free_status);
+        CHECK_EQUAL(dealloc_metadata.frame_size, 0);
+    }
+}
+#endif // !PAS_PLATFORM(PLAYSTATION)
+
 } // anonymous namespace
 
 void addPGMTests() {
@@ -374,5 +484,9 @@ void addPGMTests() {
     ADD_TEST(testPGMMetaData());
     ADD_TEST(testPGMMetadataVectorManagement());
     ADD_TEST(testPGMMetadataVectorManagementFewDeallocations());
+#if !PAS_PLATFORM(PLAYSTATION)
     ADD_TEST(testPGMMetadataDoubleFreeBehavior());
+    ADD_TEST(testPGMBmallocAllocationBacktrace());
+    ADD_TEST(testPGMAllocMetadataOnly());
+#endif // !PAS_PLATFORM(PLAYSTATION)
 }
