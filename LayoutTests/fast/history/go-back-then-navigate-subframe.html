yury-s/webkit

commit 9b13b53b41a196d0fa64076a7d0239843b4eec09

Web processes should only commit history state for navigating frames and their children https://bugs.webkit.org/show_bug.cgi?id=285224 rdar://142134339
Reviewed by Alex Christensen.

Currently, when a frame navigates, the web process creates history state for the entire frame tree to be
committed to the UI process. This needs to change with site isolation because a navigating iframe may
have parent frames that are out-of-process, which we cannot synchronously create history state for.
Instead, when a frame navigates, the web process should only create and commit history state for the
navigating frame and its children. If the navigating frame is a child frame, we can use the current item
in the UI process's back/forward list to populate the history state for its ancestors.

On navigation, only frames that navigate will be assigned a new WebBackForwardListFrameItem, while the
history item tree created for the new navigation will always receive a new WebBackForwardListItem. This
allows the UI process to correctly update the history for isolated iframes that did not navigate but had
a sibling in another process that did.

No change in behavior with site isolation disabled (hopefully). More details below.

* Source/WebCore/Headers.cmake:
* Source/WebCore/WebCore.xcodeproj/project.pbxproj:
* Source/WebCore/history/BackForwardCache.cpp:
(WebCore::BackForwardCache::addIfCacheable):
(WebCore::BackForwardCache::take):
(WebCore::BackForwardCache::get):
(WebCore::BackForwardCache::remove):
* Source/WebCore/history/BackForwardClient.h:
* Source/WebCore/history/BackForwardController.cpp:
(WebCore::BackForwardController::addItem):
(WebCore::BackForwardController::setChildItem):
* Source/WebCore/history/BackForwardController.h:
* Source/WebCore/history/BackForwardFrameItemIdentifier.h: Added.
* Source/WebCore/history/HistoryItem.cpp:
(WebCore::HistoryItem::HistoryItem):
(WebCore::HistoryItem::isInBackForwardCache const):
(WebCore::HistoryItem::hasCachedPageExpired const):

Source/WebCore/history/HistoryItem.cpp
@@ -54,12 +54,13 @@ int64_t HistoryItem::generateSequenceNumber()
     return ++next;
 }
 
-HistoryItem::HistoryItem(Client& client, const String& urlString, const String& title, const String& alternateTitle, std::optional<BackForwardItemIdentifier> identifier)
+HistoryItem::HistoryItem(Client& client, const String& urlString, const String& title, const String& alternateTitle, std::optional<BackForwardItemIdentifier> itemID, std::optional<BackForwardFrameItemIdentifier> frameItemID)
     : m_urlString(urlString)
     , m_originalURLString(urlString)
     , m_title(title)
     , m_displayTitle(alternateTitle)
-    , m_identifier(identifier ? *identifier : BackForwardItemIdentifier::generate())
+    , m_itemID(itemID ? *itemID : BackForwardItemIdentifier::generate())
+    , m_frameItemID(frameItemID ? *frameItemID : BackForwardFrameItemIdentifier::generate())
     , m_uuidIdentifier(WTF::UUID::createVersion4Weak())
     , m_client(client)
 {
@@ -88,7 +89,8 @@ HistoryItem::HistoryItem(const HistoryItem& item)
     , m_scale(item.m_scale)
     , m_scaleIsInitial(item.m_scaleIsInitial)
 #endif
-    , m_identifier(item.m_identifier)
+    , m_itemID(item.m_itemID)
+    , m_frameItemID(item.m_frameItemID)
     , m_uuidIdentifier(WTF::UUID::createVersion4Weak())
     , m_client(item.m_client)
 {
@@ -149,12 +151,12 @@ const String& HistoryItem::alternateTitle() const
 
 bool HistoryItem::isInBackForwardCache() const
 {
-    return BackForwardCache::singleton().isInBackForwardCache(m_identifier);
+    return BackForwardCache::singleton().isInBackForwardCache(m_itemID);
 }
 
 bool HistoryItem::hasCachedPageExpired() const
 {
-    return BackForwardCache::singleton().hasCachedPageExpired(m_identifier);
+    return BackForwardCache::singleton().hasCachedPageExpired(m_itemID);
 }
 
 URL HistoryItem::url() const
@@ -358,7 +360,7 @@ void HistoryItem::clearChildren()
 bool HistoryItem::shouldDoSameDocumentNavigationTo(HistoryItem& otherItem) const
 {
     // The following logic must be kept in sync with WebKit::WebBackForwardListItem::itemIsInSameDocument().
-    if (m_identifier == otherItem.identifier())
+    if (m_itemID == otherItem.itemID())
         return false;
 
     if (stateObject() || otherItem.stateObject())
@@ -463,7 +465,7 @@ int HistoryItem::showTreeWithIndent(unsigned indentLevel) const
 #if !LOG_DISABLED
 String HistoryItem::logString() const
 {
-    return makeString("HistoryItem current URL "_s, urlString(), ", identifier "_s, m_identifier.toString());
+    return makeString("HistoryItem current URL "_s, urlString(), ", identifier "_s, m_itemID.toString());
 }
 #endif
 

Source/WebCore/history/HistoryItem.h
@@ -26,6 +26,7 @@
 
 #pragma once
 
+#include "BackForwardFrameItemIdentifier.h"
 #include "BackForwardItemIdentifier.h"
 #include "FloatRect.h"
 #include "FrameIdentifier.h"
@@ -72,23 +73,24 @@ protected:
 class HistoryItem : public RefCountedAndCanMakeWeakPtr<HistoryItem> {
 public:
     using Client = HistoryItemClient;
-    static Ref<HistoryItem> create(Client& client, const String& urlString = { }, const String& title = { }, const String& alternateTitle = { }, std::optional<BackForwardItemIdentifier> identifier = { })
+    static Ref<HistoryItem> create(Client& client, const String& urlString = { }, const String& title = { }, const String& alternateTitle = { }, std::optional<BackForwardItemIdentifier> itemID = { }, std::optional<BackForwardFrameItemIdentifier> frameItemID = { })
     {
-        return adoptRef(*new HistoryItem(client, urlString, title, alternateTitle, identifier));
+        return adoptRef(*new HistoryItem(client, urlString, title, alternateTitle, itemID, frameItemID));
     }
     
     WEBCORE_EXPORT ~HistoryItem();
 
     WEBCORE_EXPORT Ref<HistoryItem> copy() const;
 
-    const BackForwardItemIdentifier& identifier() const { return m_identifier; }
+    const BackForwardItemIdentifier& itemID() const { return m_itemID; }
+    const BackForwardFrameItemIdentifier& frameItemID() const { return m_frameItemID; }
     const WTF::UUID& uuidIdentifier() const { return m_uuidIdentifier; }
     void setUUIDIdentifier(const WTF::UUID& uuidIdentifier) { m_uuidIdentifier = uuidIdentifier; }
 
     // Resets the HistoryItem to its initial state, as returned by create().
     void reset();
 
-    bool operator==(const HistoryItem& other) const { return identifier() == other.identifier(); }
+    bool operator==(const HistoryItem& other) const { return itemID() == other.itemID(); }
 
     WEBCORE_EXPORT const String& originalURLString() const;
     WEBCORE_EXPORT const String& urlString() const;
@@ -221,7 +223,7 @@ public:
     void setPolicyContainer(const PolicyContainer& policyContainer) { m_policyContainer = policyContainer; }
 
 private:
-    WEBCORE_EXPORT HistoryItem(Client&, const String& urlString, const String& title, const String& alternateTitle, std::optional<BackForwardItemIdentifier>);
+    WEBCORE_EXPORT HistoryItem(Client&, const String& urlString, const String& title, const String& alternateTitle, std::optional<BackForwardItemIdentifier>, std::optional<BackForwardFrameItemIdentifier>);
 
     HistoryItem(const HistoryItem&);
 
@@ -286,7 +288,8 @@ private:
     RetainPtr<id> m_viewState;
 #endif
 
-    BackForwardItemIdentifier m_identifier;
+    BackForwardItemIdentifier m_itemID;
+    BackForwardFrameItemIdentifier m_frameItemID;
     WTF::UUID m_uuidIdentifier;
     std::optional<PolicyContainer> m_policyContainer;
     Ref<Client> m_client;

Source/WebCore/loader/FrameLoader.cpp
@@ -2330,7 +2330,7 @@ void FrameLoader::commitProvisionalLoad()
         document->protectedEditor()->confirmOrCancelCompositionAndNotifyClient();
     }
 
-    if (!frame->tree().parent() && history().currentItem() && (!history().provisionalItem() || history().currentItem()->identifier() != history().provisionalItem()->identifier())) {
+    if (!frame->tree().parent() && history().currentItem() && (!history().provisionalItem() || history().currentItem()->itemID() != history().provisionalItem()->itemID())) {
         // Check to see if we need to cache the page we are navigating away from into the back/forward cache.
         // We are doing this here because we know for sure that a new page is about to be loaded.
         BackForwardCache::singleton().addIfCacheable(*history().protectedCurrentItem(), frame->protectedPage().get());

Source/WebCore/Headers.cmake
@@ -1261,6 +1261,7 @@ set(WebCore_PRIVATE_FRAMEWORK_HEADERS
     history/BackForwardCache.h
     history/BackForwardClient.h
     history/BackForwardController.h
+    history/BackForwardFrameItemIdentifier.h
     history/BackForwardItemIdentifier.h
     history/CachedFrame.h
     history/CachedFramePlatformData.h

Source/WebCore/history/BackForwardCache.cpp
@@ -512,13 +512,13 @@ bool BackForwardCache::addIfCacheable(HistoryItem& item, Page* page)
         // Make sure we don't fire any JS events in this scope.
         ScriptDisallowedScope::InMainThread scriptDisallowedScope;
 
-        m_cachedPageMap.set(item.identifier(), makeUniqueRefFromNonNullUniquePtr(WTFMove(cachedPage)));
-        m_items.add(item.identifier());
+        m_cachedPageMap.set(item.itemID(), makeUniqueRefFromNonNullUniquePtr(WTFMove(cachedPage)));
+        m_items.add(item.itemID());
         item.notifyChanged();
     }
     prune(PruningReason::ReachedMaxSize);
 
-    RELEASE_LOG(BackForwardCache, "BackForwardCache::addIfCacheable item: %s, size: %u / %u", item.identifier().toString().utf8().data(), pageCount(), maxSize());
+    RELEASE_LOG(BackForwardCache, "BackForwardCache::addIfCacheable item: %s, size: %u / %u", item.itemID().toString().utf8().data(), pageCount(), maxSize());
 
     return true;
 }
@@ -532,7 +532,7 @@ std::unique_ptr<CachedPage> BackForwardCache::suspendPage(Page& page)
 
 std::unique_ptr<CachedPage> BackForwardCache::take(HistoryItem& item, Page* page)
 {
-    auto it = m_cachedPageMap.find(item.identifier());
+    auto it = m_cachedPageMap.find(item.itemID());
     if (it == m_cachedPageMap.end())
         return nullptr;
     if (auto* pruningReason = std::get_if<PruningReason>(&it->value)) {
@@ -542,11 +542,11 @@ std::unique_ptr<CachedPage> BackForwardCache::take(HistoryItem& item, Page* page
         return nullptr;
     }
 
-    m_items.remove(item.identifier());
+    m_items.remove(item.itemID());
     auto cachedPage = std::get<UniqueRef<CachedPage>>(m_cachedPageMap.take(it));
     item.notifyChanged();
 
-    RELEASE_LOG(BackForwardCache, "BackForwardCache::take item: %s, size: %u / %u", item.identifier().toString().utf8().data(), pageCount(), maxSize());
+    RELEASE_LOG(BackForwardCache, "BackForwardCache::take item: %s, size: %u / %u", item.itemID().toString().utf8().data(), pageCount(), maxSize());
 
     if (cachedPage->hasExpired() || (page && page->isResourceCachingDisabledByWebInspector())) {
         LOG(BackForwardCache, "Not restoring page for %s from back/forward cache because cache entry has expired", item.url().string().ascii().data());
@@ -578,7 +578,7 @@ void BackForwardCache::removeAllItemsForPage(Page& page)
 
 CachedPage* BackForwardCache::get(HistoryItem& item, Page* page)
 {
-    auto it = m_cachedPageMap.find(item.identifier());
+    auto it = m_cachedPageMap.find(item.itemID());
     if (it == m_cachedPageMap.end())
         return nullptr;
 
@@ -613,7 +613,7 @@ void BackForwardCache::remove(BackForwardItemIdentifier itemID)
 
 void BackForwardCache::remove(HistoryItem& item)
 {
-    remove(item.identifier());
+    remove(item.itemID());
     item.notifyChanged();
 }
 

Source/WebCore/history/BackForwardClient.h
@@ -27,6 +27,7 @@
 
 #pragma once
 
+#include "BackForwardFrameItemIdentifier.h"
 #include "BackForwardItemIdentifier.h"
 #include "FrameIdentifier.h"
 #include <wtf/Forward.h>
@@ -42,8 +43,8 @@ public:
     {
     }
 
-    virtual void addItem(FrameIdentifier, Ref<HistoryItem>&&) = 0;
-    virtual void setChildItem(BackForwardItemIdentifier, Ref<HistoryItem>&&) = 0;
+    virtual void addItem(Ref<HistoryItem>&&) = 0;
+    virtual void setChildItem(BackForwardFrameItemIdentifier, Ref<HistoryItem>&&) = 0;
 
     virtual void goToItem(HistoryItem&) = 0;
     virtual void goToProvisionalItem(const HistoryItem&) = 0;

Source/WebCore/history/BackForwardController.cpp
@@ -137,14 +137,14 @@ bool BackForwardController::goForward()
     return true;
 }
 
-void BackForwardController::addItem(FrameIdentifier targetFrameID, Ref<HistoryItem>&& item)
+void BackForwardController::addItem(Ref<HistoryItem>&& item)
 {
-    protectedClient()->addItem(targetFrameID, WTFMove(item));
+    protectedClient()->addItem(WTFMove(item));
 }
 
-void BackForwardController::setChildItem(BackForwardItemIdentifier identifier, Ref<HistoryItem>&& item)
+void BackForwardController::setChildItem(BackForwardFrameItemIdentifier frameItemID, Ref<HistoryItem>&& item)
 {
-    protectedClient()->setChildItem(identifier, WTFMove(item));
+    protectedClient()->setChildItem(frameItemID, WTFMove(item));
 }
 
 void BackForwardController::setCurrentItem(HistoryItem& item)

Source/WebCore/history/BackForwardController.h
@@ -25,7 +25,7 @@
 
 #pragma once
 
-#include "BackForwardItemIdentifier.h"
+#include "BackForwardFrameItemIdentifier.h"
 #include "FrameIdentifier.h"
 #include <wtf/CheckedPtr.h>
 #include <wtf/Forward.h>
@@ -57,8 +57,8 @@ public:
     WEBCORE_EXPORT bool goBack();
     WEBCORE_EXPORT bool goForward();
 
-    void addItem(FrameIdentifier, Ref<HistoryItem>&&);
-    void setChildItem(BackForwardItemIdentifier, Ref<HistoryItem>&&);
+    void addItem(Ref<HistoryItem>&&);
+    void setChildItem(BackForwardFrameItemIdentifier, Ref<HistoryItem>&&);
     void setCurrentItem(HistoryItem&);
     void setProvisionalItem(const HistoryItem&);
     void clearProvisionalItem(const HistoryItem&);

commit 50ae75698db1c459c7bdd39d47b1e389da2495ea

REGRESSION(288409@main): Main frame history state may be incorrectly created after navigating back https://bugs.webkit.org/show_bug.cgi?id=286438 rdar://143418935
Reviewed by Alex Christensen.

FrameLoader::loadURLIntoChildFrame contains logic to reload child frames with history items from another
page when navigating through the back/forward list. This behavior caused an issue after 288409@main,
where loading a history item created by another page into a new iframe caused the UI process to create a
new main frame history item when trying to reconstruct the history item tree.

To fix this, we need to update the history item’s frameID to match the iframe that it is being loaded
into.

* LayoutTests/fast/history/go-back-then-navigate-subframe-expected.txt: Added.
* LayoutTests/fast/history/go-back-then-navigate-subframe.html: Added.
* LayoutTests/fast/history/resources/frame-final-url.html:
* LayoutTests/fast/history/resources/frame-initial-url.html:
* Source/WebCore/history/HistoryItem.cpp:
(WebCore::HistoryItem::setFrameID):
* Source/WebCore/history/HistoryItem.h:
(WebCore::HistoryItem::setFrameID): Deleted.
* Source/WebCore/loader/FrameLoader.cpp:
(WebCore::FrameLoader::loadURLIntoChildFrame):

Canonical link: https://commits.webkit.org/289329@main

Source/WebCore/history/HistoryItem.cpp
@@ -224,6 +224,12 @@ void HistoryItem::setTarget(const AtomString& target)
     notifyChanged();
 }
 
+void HistoryItem::setFrameID(std::optional<FrameIdentifier> frameID)
+{
+    m_frameID = frameID;
+    notifyChanged();
+}
+
 const IntPoint& HistoryItem::scrollPosition() const
 {
     return m_scrollPosition;

Source/WebCore/history/HistoryItem.h
@@ -137,7 +137,7 @@ public:
     WEBCORE_EXPORT void setOriginalURLString(const String&);
     WEBCORE_EXPORT void setReferrer(const String&);
     WEBCORE_EXPORT void setTarget(const AtomString&);
-    void setFrameID(std::optional<FrameIdentifier> frameID) { m_frameID = frameID; }
+    WEBCORE_EXPORT void setFrameID(std::optional<FrameIdentifier>);
     WEBCORE_EXPORT void setTitle(const String&);
     void setIsTargetItem(bool isTargetItem) { m_isTargetItem = isTargetItem; }
     

Source/WebCore/loader/FrameLoader.cpp
@@ -1058,6 +1058,7 @@ void FrameLoader::loadURLIntoChildFrame(const URL& url, const String& referer, L
     if (parentItem && parentItem->children().size() && isBackForwardLoadType(loadType()) && !m_frame->document()->loadEventFinished()) {
         if (RefPtr childItem = parentItem->childItemWithTarget(childFrame.tree().uniqueName())) {
             Ref childLoader = childFrame.loader();
+            childItem->setFrameID(childFrame.frameID());
             childLoader->m_requestedHistoryItem = childItem;
             childLoader->loadDifferentDocumentItem(*childItem, nullptr, loadType(), MayAttemptCacheOnlyLoadForFormSubmissionItem, ShouldTreatAsContinuingLoad::No);
             return;

LayoutTests/fast/history/go-back-then-navigate-subframe-expected.txt
@@ -1,0 +1,9 @@
+Tests that navigating an iframe after going back does not create a new main frame history item.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/history/go-back-then-navigate-subframe.html
@@ -1,0 +1,27 @@
+<script src="../../resources/js-test.js"></script>
+<script>
+description("Tests that navigating an iframe after going back does not create a new main frame history item.");
+jsTestIsAsync = true;
+
+function runTest() {
+    if (sessionStorage.didNavigate) {
+        delete sessionStorage.didNavigate;
+        setTimeout(() => {
+            const iframe = document.getElementById("frame");
+            iframe.src = "about:blank";
+            iframe.addEventListener("load", () => {
+                iframe.addEventListener("load", () => {
+                    finishJSTest();
+                });
+                history.back();
+            });
+        }, 0);
+    } else {
+        setTimeout(() => {
+            location.href = 'data:text/html,<script> history.back(); <\/script>';
+        }, 0);
+        sessionStorage.didNavigate = true;
+    }
+}
+</script>
+<iframe id="frame" src="resources/frame-initial-url.html#noalert"></iframe>

LayoutTests/fast/history/resources/frame-final-url.html
@@ -1,5 +1,6 @@
 final page contents
 <script>
-alert('Final URL loaded.');
+if (location.hash != "#noalert")
+    alert('Final URL loaded.');
 top.runTest();
 </script>

LayoutTests/fast/history/resources/frame-initial-url.html
@@ -1,5 +1,6 @@
 initial frame contents
 <script>
-alert('Initial URL loaded.');
-window.location='frame-final-url.html';
+if (location.hash != "#noalert")
+    alert('Initial URL loaded.');
+window.location='frame-final-url.html' + location.hash;
 </script>