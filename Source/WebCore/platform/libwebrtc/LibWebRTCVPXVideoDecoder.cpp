2c62bfaac3cff4e09f5bc024323d1a11d5049b63
Youenn Fablet <youenn@apple.com>
2025-01-29 09:35:25 -0800

    Access to vpxDecoderQueueSingleton() is not-threadsafe rdar://140577403
    Reviewed by Jean-Yves Avenard.
    
    Make use of std::call-once to initialize the queue only once.
    
    * Source/WebCore/platform/libwebrtc/LibWebRTCVPXVideoDecoder.cpp:
    (WebCore::vpxDecoderQueue):
    * Source/WebCore/platform/libwebrtc/LibWebRTCVPXVideoEncoder.cpp:
    (WebCore::vpxEncoderQueue):
    
    Originally-landed-as: 283286.533@safari-7620-branch (e975090744cf). rdar://143594246
    Canonical link: https://commits.webkit.org/289508@main

--- Source/WebCore/platform/libwebrtc/LibWebRTCVPXVideoDecoder.cpp
+++ Source/WebCore/platform/libwebrtc/LibWebRTCVPXVideoDecoder.cpp
@@ -59,7 +59,11 @@ WTF_MAKE_TZONE_ALLOCATED_IMPL(LibWebRTCVPXVideoDecoder);
 
 static WorkQueue& vpxDecoderQueueSingleton()
 {
-    static NeverDestroyed<Ref<WorkQueue>> queue(WorkQueue::create("VPx VideoDecoder Queue"_s));
+    static std::once_flag onceKey;
+    static LazyNeverDestroyed<Ref<WorkQueue>> queue;
+    std::call_once(onceKey, [] {
+        queue.construct(WorkQueue::create("VPx VideoDecoder Queue"_s));
+    });
     return queue.get();
 }
 
