39cfdb0bd48e7537e24f772d0f9db97a51c9138a
Charlie Wolfe <charliew@apple.com>
2025-01-31 16:59:50 -0800

    NetworkConnectionToWebProcess::performSynchronousLoad should have a Data Isolation check https://bugs.webkit.org/show_bug.cgi?id=284434 rdar://141255904
    Reviewed by Alex Christensen.
    
    * Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp:
    (WebKit::NetworkConnectionToWebProcess::performSynchronousLoad):
    
    Originally-landed-as: 283286.585@safari-7620-branch (dc56db7312eb). rdar://143592810
    Canonical link: https://commits.webkit.org/289645@main

--- Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp
+++ Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp
@@ -595,6 +595,7 @@ void NetworkConnectionToWebProcess::scheduleResourceLoad(NetworkResourceLoadPara
 
 void NetworkConnectionToWebProcess::performSynchronousLoad(NetworkResourceLoadParameters&& loadParameters, CompletionHandler<void(const ResourceError&, const ResourceResponse, Vector<uint8_t>&&)>&& reply)
 {
+    MESSAGE_CHECK(m_networkProcess->allowsFirstPartyForCookies(m_webProcessIdentifier, loadParameters.request.firstPartyForCookies()) == NetworkProcess::AllowCookieAccess::Allow);
     CONNECTION_RELEASE_LOG(Loading, "performSynchronousLoad: (parentPID=%d, pageProxyID=%" PRIu64 ", webPageID=%" PRIu64 ", frameID=%" PRIu64 ", resourceID=%" PRIu64 ")", loadParameters.parentPID, loadParameters.webPageProxyID ? loadParameters.webPageProxyID->toUInt64() : 0, loadParameters.webPageID ? loadParameters.webPageID->toUInt64() : 0, loadParameters.webFrameID ? loadParameters.webFrameID->object().toUInt64() : 0, loadParameters.identifier ? loadParameters.identifier->toUInt64() : 0);
 
     auto identifier = loadParameters.identifier;
