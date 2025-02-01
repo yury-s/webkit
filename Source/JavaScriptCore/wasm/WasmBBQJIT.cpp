dff14391ec0349aba5b843488112cc21b7b492cc
David Degazio <d_degazio@apple.com>
2025-01-31 17:49:04 -0800

    [WASM] Tail calls should consume expression stack after call in BBQ https://bugs.webkit.org/show_bug.cgi?id=284159 rdar://141164805
    Reviewed by Yusuke Suzuki.
    
    Adds a loop to consume expression stack elements after we
    generate a tail call (direct or indirect) in BBQ. This is
    consistent with our behavior in the return opcode, and
    prevents register bindings from persisting into the
    subsequent unreachable code.
    
    * JSTests/wasm/stress/tail-call-should-consume-stack-in-bbq.js: Added.
    * Source/JavaScriptCore/wasm/WasmBBQJIT.cpp:
    (JSC::Wasm::BBQJITImpl::BBQJIT::emitTailCall):
    (JSC::Wasm::BBQJITImpl::BBQJIT::emitIndirectTailCall):
    
    Originally-landed-as: 283286.575@safari-7620-branch (ca83d835b014). rdar://143593119
    Canonical link: https://commits.webkit.org/289650@main

--- Source/JavaScriptCore/wasm/WasmBBQJIT.cpp
+++ Source/JavaScriptCore/wasm/WasmBBQJIT.cpp
@@ -4141,7 +4141,6 @@ void BBQJIT::emitTailCall(FunctionSpaceIndex functionIndex, const TypeDefinition
         else
             resolvedArguments.append(Value::pinned(arguments[i].type(), locationOf(arguments[i])));
 
-        // This isn't really needed but it's nice to have good book keeping.
         consume(arguments[i]);
     }
 
@@ -4198,6 +4197,13 @@ void BBQJIT::emitTailCall(FunctionSpaceIndex functionIndex, const TypeDefinition
     }
 
     LOG_INSTRUCTION("ReturnCall", functionIndex, arguments);
+
+    // Because we're returning, we need to consume all elements of the
+    // expression stack here so they don't spuriously hold onto their bindings
+    // in the subsequent unreachable code.
+
+    for (const auto& value : m_parser->expressionStack())
+        consume(value);
 }
 
 
@@ -4409,6 +4415,13 @@ void BBQJIT::emitIndirectTailCall(const char* opcode, const Value& callee, GPRRe
 
     m_jit.farJump(calleeCode, WasmEntryPtrTag);
     LOG_INSTRUCTION(opcode, callee, arguments);
+
+    // Because we're returning, we need to consume all elements of the
+    // expression stack here so they don't spuriously hold onto their bindings
+    // in the subsequent unreachable code.
+
+    for (const auto& value : m_parser->expressionStack())
+        consume(value);
 }
 
 void BBQJIT::addRTTSlowPathJump(TypeIndex signature, GPRReg calleeRTT)
