yury-s/webkit

commit 842d24a827ff7ba2e3ecdcbe6665b0a826d5c4e6

LibWebRTCMediaEndpoint::createStatsCollector should enqueue a task to resolve the getStats promise rdar://140621793 https://bugs.webkit.org/show_bug.cgi?id=283753
Reviewed by Jean-Yves Avenard.

We enqueue a task to resolve the promise as done for other promises.

* Source/WebCore/Modules/mediastream/libwebrtc/LibWebRTCMediaEndpoint.cpp:
(WebCore::LibWebRTCMediaEndpoint::createStatsCollector):

Canonical link: https://commits.webkit.org/287147@main

Source/WebCore/Modules/mediastream/libwebrtc/LibWebRTCMediaEndpoint.cpp
@@ -275,7 +275,11 @@ rtc::scoped_refptr<LibWebRTCStatsCollector> LibWebRTCMediaEndpoint::createStatsC
         if (protectedThis->isStopped())
             return;
 
-        promise->resolve<IDLInterface<RTCStatsReport>>(LibWebRTCStatsCollector::createReport(rtcReport));
+        Ref peerConnectionBackend = protectedThis->m_peerConnectionBackend.get();
+        Ref peerConnection = peerConnectionBackend->connection();
+        peerConnection->queueTaskKeepingObjectAlive(peerConnection.get(), TaskSource::Networking, [promise = WTFMove(promise), rtcReport = WTFMove(rtcReport)] {
+            promise->resolve<IDLInterface<RTCStatsReport>>(LibWebRTCStatsCollector::createReport(rtcReport));
+        });
     });
 }
 

commit 80245993fa5ad38a289984f5a377c78384fcaae7

[LibWebRTC] Fix Linux, GStreamer and unified build https://bugs.webkit.org/show_bug.cgi?id=286816
Reviewed by Philippe Normand.

* Source/ThirdParty/libwebrtc/CMakeLists.txt:
  - Remove test support files requiring gtest headers, not exposed in CMake build
    (introduced in libwebrtc M132 update, 287962@main)

* Source/WebCore/Modules/mediastream/libwebrtc/LibWebRTCMediaEndpoint.cpp:
(WebCore::LibWebRTCMediaEndpoint::createStatsCollector):
  - Do not move rtcReport to avoid const qualified error (introduced in 287147@main)

* Source/WebCore/platform/mediastream/libwebrtc/gstreamer/GStreamerVideoEncoderFactory.cpp:
(WebCore::GStreamerVideoEncoder::makeElement):
  - Use ASCIILiteral to avoid error from missing span(const gchar*) (from 279038@main)

* Source/WebKit/WebProcess/Network/webrtc/WebRTCMonitor.cpp:
  - Include missing LibWebRTCNetwork.h for ref/deref methods

Canonical link: https://commits.webkit.org/289776@main

Source/ThirdParty/libwebrtc/CMakeLists.txt
@@ -887,7 +887,6 @@ set(webrtc_SOURCES
     Source/webrtc/media/base/codec_comparators.cc
     Source/webrtc/media/base/fake_frame_source.cc
     Source/webrtc/media/base/fake_media_engine.cc
-    Source/webrtc/media/base/fake_rtp.cc
     Source/webrtc/media/base/fake_video_renderer.cc
     Source/webrtc/media/base/media_channel_impl.cc
     Source/webrtc/media/base/media_constants.cc
@@ -1543,7 +1542,6 @@ set(webrtc_SOURCES
     Source/webrtc/pc/peer_connection.cc
     Source/webrtc/pc/peer_connection_factory.cc
     Source/webrtc/pc/peer_connection_message_handler.cc
-    Source/webrtc/pc/peer_connection_wrapper.cc
     Source/webrtc/pc/remote_audio_source.cc
     Source/webrtc/pc/rtc_stats_collector.cc
     Source/webrtc/pc/rtc_stats_traversal.cc
@@ -1757,17 +1755,13 @@ set(webrtc_SOURCES
     Source/webrtc/video/report_block_stats.cc
     Source/webrtc/video/rtp_streams_synchronizer2.cc
     Source/webrtc/video/rtp_video_stream_receiver2.cc
-    Source/webrtc/video/screenshare_loopback.cc
     Source/webrtc/video/send_delay_stats.cc
     Source/webrtc/video/send_statistics_proxy.cc
     Source/webrtc/video/stats_counter.cc
     Source/webrtc/video/stream_synchronization.cc
-    Source/webrtc/video/sv_loopback.cc
     Source/webrtc/video/task_queue_frame_decode_scheduler.cc
     Source/webrtc/video/transport_adapter.cc
     Source/webrtc/video/unique_timestamp_counter.cc
-    Source/webrtc/video/video_analyzer.cc
-    Source/webrtc/video/video_loopback.cc
     Source/webrtc/video/video_loopback_main.cc
     Source/webrtc/video/video_quality_observer2.cc
     Source/webrtc/video/video_receive_stream2.cc

Source/WebCore/Modules/mediastream/libwebrtc/LibWebRTCMediaEndpoint.cpp
@@ -279,7 +279,7 @@ rtc::scoped_refptr<LibWebRTCStatsCollector> LibWebRTCMediaEndpoint::createStatsC
 
         Ref peerConnectionBackend = protectedThis->m_peerConnectionBackend.get();
         Ref peerConnection = peerConnectionBackend->connection();
-        peerConnection->queueTaskKeepingObjectAlive(peerConnection.get(), TaskSource::Networking, [promise = WTFMove(promise), rtcReport = WTFMove(rtcReport)] {
+        peerConnection->queueTaskKeepingObjectAlive(peerConnection.get(), TaskSource::Networking, [promise = WTFMove(promise), rtcReport] {
             promise->resolve<IDLInterface<RTCStatsReport>>(LibWebRTCStatsCollector::createReport(rtcReport));
         });
     });

Source/WebCore/platform/mediastream/libwebrtc/gstreamer/GStreamerVideoEncoderFactory.cpp
@@ -121,7 +121,7 @@ public:
     GstElement* makeElement(const gchar* factoryName)
     {
         static Atomic<uint32_t> elementId;
-        auto name = makeString(span(Name()), "-enc-"_s, unsafeSpan(factoryName), "-"_s, elementId.exchangeAdd(1));
+        auto name = makeString(Name(), "-enc-"_s, unsafeSpan(factoryName), "-"_s, elementId.exchangeAdd(1));
         auto* elem = makeGStreamerElement(factoryName, name.utf8().data());
         return elem;
     }
@@ -317,7 +317,7 @@ public:
     }
 
     virtual webrtc::VideoCodecType CodecType() = 0;
-    virtual const gchar* Name() = 0;
+    virtual ASCIILiteral Name() = 0;
     virtual webrtc::SdpVideoFormat sdpVideoFormat() = 0;
     virtual int KeyframeInterval(const webrtc::VideoCodec* codecSettings) = 0;
 
@@ -358,7 +358,7 @@ public:
     }
 
     const gchar* Caps() final { return "video/x-h264"; }
-    const gchar* Name() final { return "h264"; }
+    ASCIILiteral Name() final { return "h264"_s; }
     webrtc::SdpVideoFormat sdpVideoFormat() final { return webrtc::SdpVideoFormat::H264(); }
     webrtc::VideoCodecType CodecType() final { return webrtc::kVideoCodecH264; }
 };

Source/WebKit/WebProcess/Network/webrtc/WebRTCMonitor.cpp
@@ -28,6 +28,7 @@
 
 #if USE(LIBWEBRTC)
 
+#include "LibWebRTCNetwork.h"
 #include "Logging.h"
 #include "NetworkConnectionToWebProcessMessages.h"
 #include "NetworkProcessConnection.h"