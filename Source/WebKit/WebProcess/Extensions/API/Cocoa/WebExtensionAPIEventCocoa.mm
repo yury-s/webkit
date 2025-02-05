yury-s/webkit

commit 9936ea82683e58a49ade3958b0352782bbd01bcc

Send browser.test messages to all extension content world types https://bugs.webkit.org/show_bug.cgi?id=286158 rdar://143142956
Reviewed by Timothy Hatcher.

This patch fixes a bug where browser.test.onMessage event listeners never
received events in web pages or content scripts. This was happening because
browser.test messages were only being sent to the main world.

We can fix this by sending the messages to all extension content world types.

* Source/WebKit/UIProcess/Extensions/Cocoa/WebExtensionContextCocoa.mm:
(WebKit::WebExtensionContext::sendTestMessage):
Send test messages to main, web page, and content script extension content
world types.

* Source/WebKit/WebProcess/Extensions/API/Cocoa/WebExtensionAPIEventCocoa.mm:
(WebKit::WebExtensionAPIEvent::addListener):
(WebKit::WebExtensionAPIEvent::removeListener):
(WebKit::WebExtensionAPIEvent::removeAllListeners):
Don't bail out early if we don't have an extension context (i.e. web page)
and we're in test mode. In this case, add the listener to all extension
contexts (and add the frame to the extension context).

* Source/WebKit/WebProcess/Extensions/API/Cocoa/WebExtensionAPITestCocoa.mm:
(WebKit::WebExtensionContextProxy::dispatchTestMessageEvent):
Enumerate the web page namespace objects if the content world type is web page.

* Source/WebKit/WebProcess/Extensions/WebExtensionContextProxy.cpp:
(WebKit::WebExtensionContextProxy::enumerateFramesAndWebPageNamespaceObjects):
Introduce a new method to enumerate web page namespace object.

* Source/WebKit/WebProcess/Extensions/WebExtensionContextProxy.h:
* Source/WebKit/WebProcess/Extensions/WebExtensionContextProxy.messages.in:
Send the content world type when sending a test message.

* Tools/TestWebKitAPI/Tests/WebKitCocoa/WKWebExtensionAPITest.mm:
(TestWebKitAPI::TEST(WKWebExtensionAPITest, MessageEventInWebPage)):
(TestWebKitAPI::TEST(WKWebExtensionAPITest, MessageEventInContentScript)):
Write new API tests.

Source/WebKit/WebProcess/Extensions/API/Cocoa/WebExtensionAPIEventCocoa.mm
@@ -32,6 +32,9 @@
 
 #import "MessageSenderInlines.h"
 #import "WebExtensionContextMessages.h"
+#import "WebExtensionControllerProxy.h"
+#import "WebFrame.h"
+#import "WebPage.h"
 #import "WebProcess.h"
 #import <JavaScriptCore/APICast.h>
 #import <JavaScriptCore/ScriptCallStack.h>
@@ -96,8 +99,19 @@ void WebExtensionAPIEvent::addListener(WebCore::FrameIdentifier frameIdentifier,
     m_frameIdentifier = frameIdentifier;
     m_listeners.append(listener);
 
-    if (!hasExtensionContext())
+    RefPtr webFrame = WebProcess::singleton().webFrame(frameIdentifier);
+    RefPtr webExtensionControllerProxy = webFrame->page()->webExtensionControllerProxy();
+
+    if (!hasExtensionContext()) {
+        if (webExtensionControllerProxy->inTestingMode()) {
+            for (Ref extensionContext : webExtensionControllerProxy->extensionContexts()) {
+                extensionContext->addFrameWithExtensionContent(*webFrame);
+                WebProcess::singleton().send(Messages::WebExtensionContext::AddListener(*m_frameIdentifier, m_type, contentWorldType()), extensionContext->identifier());
+            }
+        }
+
         return;
+    }
 
     WebProcess::singleton().send(Messages::WebExtensionContext::AddListener(*m_frameIdentifier, m_type, contentWorldType()), extensionContext().identifier());
 }
@@ -113,8 +127,17 @@ void WebExtensionAPIEvent::removeListener(WebCore::FrameIdentifier frameIdentifi
 
     ASSERT(frameIdentifier == m_frameIdentifier);
 
-    if (!hasExtensionContext())
+    RefPtr webFrame = WebProcess::singleton().webFrame(frameIdentifier);
+    RefPtr webExtensionControllerProxy = webFrame->page()->webExtensionControllerProxy();
+
+    if (!hasExtensionContext()) {
+        if (webExtensionControllerProxy->inTestingMode()) {
+            for (Ref extensionContext : webExtensionControllerProxy->extensionContexts())
+                WebProcess::singleton().send(Messages::WebExtensionContext::RemoveListener(*m_frameIdentifier, m_type, contentWorldType(), removedCount), extensionContext->identifier());
+        }
+
         return;
+    }
 
     WebProcess::singleton().send(Messages::WebExtensionContext::RemoveListener(*m_frameIdentifier, m_type, contentWorldType(), removedCount), extensionContext().identifier());
 }
@@ -134,8 +157,17 @@ void WebExtensionAPIEvent::removeAllListeners()
     auto removedCount = m_listeners.size();
     m_listeners.clear();
 
-    if (!hasExtensionContext())
+    RefPtr webFrame = WebProcess::singleton().webFrame(m_frameIdentifier);
+    RefPtr webExtensionControllerProxy = webFrame->page()->webExtensionControllerProxy();
+
+    if (!hasExtensionContext()) {
+        if (webExtensionControllerProxy->inTestingMode()) {
+            for (Ref extensionContext : webExtensionControllerProxy->extensionContexts())
+                WebProcess::singleton().send(Messages::WebExtensionContext::RemoveListener(*m_frameIdentifier, m_type, contentWorldType(), removedCount), extensionContext->identifier());
+        }
+
         return;
+    }
 
     WebProcess::singleton().send(Messages::WebExtensionContext::RemoveListener(*m_frameIdentifier, m_type, contentWorldType(), removedCount), extensionContext().identifier());
 }

Source/WebKit/UIProcess/Extensions/Cocoa/WebExtensionContextCocoa.mm
@@ -4989,8 +4989,13 @@ void WebExtensionContext::sendTestMessage(const String& message, id argument)
     String argumentJSON = encodeJSONString(argument, JSONOptions::FragmentsAllowed);
 
     constexpr auto eventType = WebExtensionEventListenerType::TestOnMessage;
+
+    sendToProcesses(processes(eventType, WebExtensionContentWorldType::WebPage), Messages::WebExtensionContextProxy::DispatchTestMessageEvent(message, argumentJSON, WebExtensionContentWorldType::WebPage));
+
+    sendToContentScriptProcessesForEvent(eventType, Messages::WebExtensionContextProxy::DispatchTestMessageEvent(message, argumentJSON, WebExtensionContentWorldType::ContentScript));
+
     wakeUpBackgroundContentIfNecessaryToFireEvents({ eventType }, [=, this, protectedThis = Ref { *this }] {
-        sendToProcessesForEvent(eventType, Messages::WebExtensionContextProxy::DispatchTestMessageEvent(message, argumentJSON));
+        sendToProcessesForEvent(eventType, Messages::WebExtensionContextProxy::DispatchTestMessageEvent(message, argumentJSON, WebExtensionContentWorldType::Main));
     });
 }
 

Source/WebKit/WebProcess/Extensions/API/Cocoa/WebExtensionAPITestCocoa.mm
@@ -33,6 +33,7 @@
 #import "CocoaHelpers.h"
 #import "MessageSenderInlines.h"
 #import "WebExtensionAPINamespace.h"
+#import "WebExtensionAPIWebPageNamespace.h"
 #import "WebExtensionControllerMessages.h"
 #import "WebExtensionControllerProxy.h"
 #import "WebExtensionEventListenerType.h"
@@ -345,13 +346,21 @@ JSValue *WebExtensionAPITest::assertSafeResolve(JSContextRef context, JSValue *f
     return assertResolves(context, result, message);
 }
 
-void WebExtensionContextProxy::dispatchTestMessageEvent(const String& message, const String& argumentJSON)
+void WebExtensionContextProxy::dispatchTestMessageEvent(const String& message, const String& argumentJSON, WebExtensionContentWorldType contentWorldType)
 {
     id argument = parseJSON(argumentJSON, JSONOptions::FragmentsAllowed);
 
-    enumerateNamespaceObjects([&](auto& namespaceObject) {
+    if (contentWorldType == WebExtensionContentWorldType::WebPage) {
+        enumerateFramesAndWebPageNamespaceObjects([&](auto&, auto& namespaceObject) {
+            namespaceObject.test().onMessage().invokeListenersWithArgument(message, argument);
+        });
+
+        return;
+    }
+
+    enumerateFramesAndNamespaceObjects([&](auto&, auto& namespaceObject) {
         namespaceObject.test().onMessage().invokeListenersWithArgument(message, argument);
-    });
+    }, toDOMWrapperWorld(contentWorldType));
 }
 
 } // namespace WebKit

Source/WebKit/WebProcess/Extensions/Cocoa/WebExtensionContextProxyCocoa.mm
@@ -182,7 +182,7 @@ RefPtr<WebExtensionLocalization> WebExtensionContextProxy::parseLocalization(Ref
     return nullptr;
 }
 
-WebCore::DOMWrapperWorld& WebExtensionContextProxy::toDOMWrapperWorld(WebExtensionContentWorldType contentWorldType) const
+Ref<WebCore::DOMWrapperWorld> WebExtensionContextProxy::toDOMWrapperWorld(WebExtensionContentWorldType contentWorldType) const
 {
     switch (contentWorldType) {
     case WebExtensionContentWorldType::Main:

Source/WebKit/WebProcess/Extensions/WebExtensionContextProxy.cpp
@@ -29,8 +29,10 @@
 #if ENABLE(WK_WEB_EXTENSIONS)
 
 #include "JSWebExtensionAPINamespace.h"
+#include "JSWebExtensionAPIWebPageNamespace.h"
 #include "JSWebExtensionWrapper.h"
 #include "WebExtensionAPINamespace.h"
+#include "WebExtensionAPIWebPageNamespace.h"
 #include "WebExtensionControllerProxy.h"
 #include "WebFrame.h"
 #include "WebPage.h"
@@ -183,7 +185,7 @@ void WebExtensionContextProxy::setStorageAccessLevel(bool allowedInContentScript
     m_isSessionStorageAllowedInContentScripts = allowedInContentScripts;
 }
 
-void WebExtensionContextProxy::enumerateFramesAndNamespaceObjects(const Function<void(WebFrame&, WebExtensionAPINamespace&)>& function, DOMWrapperWorld& world)
+void WebExtensionContextProxy::enumerateFramesAndNamespaceObjects(const Function<void(WebFrame&, WebExtensionAPINamespace&)>& function, Ref<DOMWrapperWorld>&& world)
 {
     m_extensionContentFrames.forEach([&](auto& frame) {
         RefPtr page = frame.page() ? frame.page()->corePage() : nullptr;
@@ -203,6 +205,24 @@ void WebExtensionContextProxy::enumerateFramesAndNamespaceObjects(const Function
             if (chromeNamespaceObject && JSValueIsObject(context, chromeNamespaceObject))
                 namespaceObjectImpl = toWebExtensionAPINamespace(context, chromeNamespaceObject);
         }
+
+        if (!namespaceObjectImpl)
+            return;
+
+        function(frame, *namespaceObjectImpl);
+    });
+}
+
+void WebExtensionContextProxy::enumerateFramesAndWebPageNamespaceObjects(const Function<void(WebFrame&, WebExtensionAPIWebPageNamespace&)>& function)
+{
+    m_extensionContentFrames.forEach([&](auto& frame) {
+        auto context = frame.jsContextForWorld(mainWorldSingleton());
+        auto globalObject = JSContextGetGlobalObject(context);
+
+        RefPtr<WebExtensionAPIWebPageNamespace> namespaceObjectImpl;
+        auto browserNamespaceObject = JSObjectGetProperty(context, globalObject, toJSString("browser").get(), nullptr);
+        if (browserNamespaceObject && JSValueIsObject(context, browserNamespaceObject))
+            namespaceObjectImpl = toWebExtensionAPIWebPageNamespace(context, browserNamespaceObject);
 
         if (!namespaceObjectImpl)
             return;

Source/WebKit/WebProcess/Extensions/WebExtensionContextProxy.h
@@ -45,6 +45,7 @@ namespace WebKit {
 
 class WebExtensionAPINamespace;
 class WebExtensionAPIStorage;
+class WebExtensionAPIWebPageNamespace;
 class WebExtensionControllerProxy;
 class WebExtensionMatchPattern;
 class WebFrame;
@@ -93,7 +94,7 @@ public:
     bool inTestingMode() const;
 
     bool hasDOMWrapperWorld(WebExtensionContentWorldType contentWorldType) const { return contentWorldType != WebExtensionContentWorldType::ContentScript || hasContentScriptWorld(); }
-    WebCore::DOMWrapperWorld& toDOMWrapperWorld(WebExtensionContentWorldType) const;
+    Ref<WebCore::DOMWrapperWorld> toDOMWrapperWorld(WebExtensionContentWorldType) const;
 
     static WebCore::DOMWrapperWorld& mainWorldSingleton() { return WebCore::mainThreadNormalWorld(); }
 
@@ -128,7 +129,8 @@ public:
     Vector<Ref<WebPage>> tabPages(std::optional<WebExtensionTabIdentifier> = std::nullopt, std::optional<WebExtensionWindowIdentifier> = std::nullopt) const;
     void addTabPage(WebPage&, std::optional<WebExtensionTabIdentifier>, std::optional<WebExtensionWindowIdentifier>);
 
-    void enumerateFramesAndNamespaceObjects(const Function<void(WebFrame&, WebExtensionAPINamespace&)>&, WebCore::DOMWrapperWorld& = mainWorldSingleton());
+    void enumerateFramesAndNamespaceObjects(const Function<void(WebFrame&, WebExtensionAPINamespace&)>&, Ref<WebCore::DOMWrapperWorld>&& = mainWorldSingleton());
+    void enumerateFramesAndWebPageNamespaceObjects(const Function<void(WebFrame&, WebExtensionAPIWebPageNamespace&)>&);
 
     void enumerateNamespaceObjects(const Function<void(WebExtensionAPINamespace&)>& function)
     {
@@ -205,7 +207,7 @@ private:
     void dispatchTabsRemovedEvent(WebExtensionTabIdentifier, WebExtensionWindowIdentifier, WebExtensionContext::WindowIsClosing);
 
     // Test
-    void dispatchTestMessageEvent(const String& message, const String& argumentJSON);
+    void dispatchTestMessageEvent(const String& message, const String& argumentJSON, WebExtensionContentWorldType);
 
     // Web Navigation
     void dispatchWebNavigationEvent(WebExtensionEventListenerType, WebExtensionTabIdentifier, const WebExtensionFrameParameters&, WallTime);

Source/WebKit/WebProcess/Extensions/WebExtensionContextProxy.messages.in
@@ -91,7 +91,7 @@ messages -> WebExtensionContextProxy {
     DispatchTabsRemovedEvent(WebKit::WebExtensionTabIdentifier tabIdentifier, WebKit::WebExtensionWindowIdentifier windowIdentifier, WebKit::WebExtensionContext::WindowIsClosing windowIsClosing)
 
     // Test Events
-    DispatchTestMessageEvent(String message, String argumentJSON)
+    DispatchTestMessageEvent(String message, String argumentJSON, WebKit::WebExtensionContentWorldType contentWorldType)
 
     // Web Navigation
     DispatchWebNavigationEvent(WebKit::WebExtensionEventListenerType type, WebKit::WebExtensionTabIdentifier tabID, struct WebKit::WebExtensionFrameParameters frameParamaters, WallTime timestamp)

Tools/TestWebKitAPI/Tests/WebKitCocoa/WKWebExtensionAPITest.mm
@@ -41,7 +41,12 @@ static auto *manifest = @{
     @"background": @{
         @"scripts": @[ @"background.js" ],
         @"persistent": @NO
-    }
+    },
+
+    @"content_scripts": @[@{
+        @"matches": @[ @"*://*/*" ],
+        @"js": @[ @"content.js" ]
+    }]
 };
 
 TEST(WKWebExtensionAPITest, MessageEvent)
@@ -60,6 +65,81 @@ TEST(WKWebExtensionAPITest, MessageEvent)
     auto manager = Util::loadExtension(manifest, @{ @"background.js": backgroundScript });
 
     [manager runUntilTestMessage:@"Send Test Message"];
+
+    [manager sendTestMessage:@"Test" withArgument:@{ @"key": @"value" }];
+
+    [manager run];
+}
+
+TEST(WKWebExtensionAPITest, MessageEventInWebPage)
+{
+    auto *pageScript = Util::constructScript(@[
+        @"browser.test.onMessage.addListener((message, data) => {",
+        @"  browser.test.assertEq(message, 'Test', 'message should be')",
+        @"  browser.test.assertEq(data?.key, 'value', 'data.key should be')",
+
+        @"  browser.test.notifyPass()",
+        @"})",
+
+        @"browser.test.sendMessage('Ready for Message')"
+    ]);
+
+    TestWebKitAPI::HTTPServer server({
+        { "/"_s, { { { "Content-Type"_s, "text/html"_s } }, "<script type='module' src='script.js'></script>"_s } },
+        { "/script.js"_s, { { { "Content-Type"_s, "application/javascript"_s } }, pageScript } }
+    }, TestWebKitAPI::HTTPServer::Protocol::Http);
+
+    auto *resources = @{
+        @"background.js": @"// This script is intentionally left blank."
+    };
+
+    auto extension = adoptNS([[WKWebExtension alloc] _initWithManifestDictionary:manifest resources:resources]);
+    auto manager = adoptNS([[TestWebExtensionManager alloc] initForExtension:extension.get()]);
+
+    [manager load];
+
+    [manager.get().defaultTab.webView loadRequest:server.requestWithLocalhost()];
+
+    [manager runUntilTestMessage:@"Ready for Message"];
+
+    [manager sendTestMessage:@"Test" withArgument:@{ @"key": @"value" }];
+
+    [manager run];
+}
+
+TEST(WKWebExtensionAPITest, MessageEventInContentScript)
+{
+    TestWebKitAPI::HTTPServer server({
+        { "/"_s, { { { "Content-Type"_s, "text/html"_s } }, ""_s } }
+    }, TestWebKitAPI::HTTPServer::Protocol::Http);
+
+    auto *contentScript = Util::constructScript(@[
+        @"browser.test.onMessage.addListener((message, data) => {",
+        @"  browser.test.assertEq(message, 'Test', 'message should be')",
+        @"  browser.test.assertEq(data?.key, 'value', 'data.key should be')",
+
+        @"  browser.test.notifyPass()",
+        @"})",
+
+        @"browser.test.sendMessage('Ready for Message')"
+    ]);
+
+    auto *resources = @{
+        @"background.js": @"// This script is intentionally left blank.",
+        @"content.js": contentScript
+    };
+
+    auto extension = adoptNS([[WKWebExtension alloc] _initWithManifestDictionary:manifest resources:resources]);
+    auto manager = adoptNS([[TestWebExtensionManager alloc] initForExtension:extension.get()]);
+
+    auto *urlRequest = server.requestWithLocalhost();
+    [manager.get().context setPermissionStatus:WKWebExtensionContextPermissionStatusGrantedExplicitly forURL:urlRequest.URL];
+
+    [manager load];
+
+    [manager.get().defaultTab.webView loadRequest:urlRequest];
+
+    [manager runUntilTestMessage:@"Ready for Message"];
 
     [manager sendTestMessage:@"Test" withArgument:@{ @"key": @"value" }];
 

commit 7f8613c028df85e2bde63c58c44a492061879148

Crash in com.apple.WebKit.WebContent at com.apple.WebKit: WebKit::WebExtensionAPIEvent::removeAllListeners https://bugs.webkit.org/show_bug.cgi?id=287095 rdar://144211832
Reviewed by Timothy Hatcher and Brian Weinstein.

This patch is a speculative fix for a crash introduced by 289222@main, as I
have been unable to reproduce the problem.

The crash is almost certainly happening because we're dereferencing a nullptr,
so the fix is to simply add null checks prior to dereferencing.

* Source/WebKit/WebProcess/Extensions/API/Cocoa/WebExtensionAPIEventCocoa.mm:
(WebKit::WebExtensionAPIEvent::addListener):
(WebKit::WebExtensionAPIEvent::removeListener):
(WebKit::WebExtensionAPIEvent::removeAllListeners):

Canonical link: https://commits.webkit.org/289898@main

Source/WebKit/WebProcess/Extensions/API/Cocoa/WebExtensionAPIEventCocoa.mm
@@ -99,11 +99,12 @@ void WebExtensionAPIEvent::addListener(WebCore::FrameIdentifier frameIdentifier,
     m_frameIdentifier = frameIdentifier;
     m_listeners.append(listener);
 
-    RefPtr webFrame = WebProcess::singleton().webFrame(frameIdentifier);
-    RefPtr webExtensionControllerProxy = webFrame->page()->webExtensionControllerProxy();
-
     if (!hasExtensionContext()) {
-        if (webExtensionControllerProxy->inTestingMode()) {
+        RefPtr webFrame = WebProcess::singleton().webFrame(m_frameIdentifier);
+        RefPtr webPage = webFrame ? webFrame->page() : nullptr;
+        RefPtr webExtensionControllerProxy = webPage ? webPage->webExtensionControllerProxy() : nullptr;
+
+        if (webExtensionControllerProxy && webExtensionControllerProxy->inTestingMode()) {
             for (Ref extensionContext : webExtensionControllerProxy->extensionContexts()) {
                 extensionContext->addFrameWithExtensionContent(*webFrame);
                 WebProcess::singleton().send(Messages::WebExtensionContext::AddListener(*m_frameIdentifier, m_type, contentWorldType()), extensionContext->identifier());
@@ -127,11 +128,12 @@ void WebExtensionAPIEvent::removeListener(WebCore::FrameIdentifier frameIdentifi
 
     ASSERT(frameIdentifier == m_frameIdentifier);
 
-    RefPtr webFrame = WebProcess::singleton().webFrame(frameIdentifier);
-    RefPtr webExtensionControllerProxy = webFrame->page()->webExtensionControllerProxy();
-
     if (!hasExtensionContext()) {
-        if (webExtensionControllerProxy->inTestingMode()) {
+        RefPtr webFrame = WebProcess::singleton().webFrame(m_frameIdentifier);
+        RefPtr webPage = webFrame ? webFrame->page() : nullptr;
+        RefPtr webExtensionControllerProxy = webPage ? webPage->webExtensionControllerProxy() : nullptr;
+
+        if (webExtensionControllerProxy && webExtensionControllerProxy->inTestingMode()) {
             for (Ref extensionContext : webExtensionControllerProxy->extensionContexts())
                 WebProcess::singleton().send(Messages::WebExtensionContext::RemoveListener(*m_frameIdentifier, m_type, contentWorldType(), removedCount), extensionContext->identifier());
         }
@@ -157,11 +159,12 @@ void WebExtensionAPIEvent::removeAllListeners()
     auto removedCount = m_listeners.size();
     m_listeners.clear();
 
-    RefPtr webFrame = WebProcess::singleton().webFrame(m_frameIdentifier);
-    RefPtr webExtensionControllerProxy = webFrame->page()->webExtensionControllerProxy();
-
     if (!hasExtensionContext()) {
-        if (webExtensionControllerProxy->inTestingMode()) {
+        RefPtr webFrame = WebProcess::singleton().webFrame(m_frameIdentifier);
+        RefPtr webPage = webFrame ? webFrame->page() : nullptr;
+        RefPtr webExtensionControllerProxy = webPage ? webPage->webExtensionControllerProxy() : nullptr;
+
+        if (webExtensionControllerProxy && webExtensionControllerProxy->inTestingMode()) {
             for (Ref extensionContext : webExtensionControllerProxy->extensionContexts())
                 WebProcess::singleton().send(Messages::WebExtensionContext::RemoveListener(*m_frameIdentifier, m_type, contentWorldType(), removedCount), extensionContext->identifier());
         }